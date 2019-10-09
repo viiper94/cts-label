dbcb2df1046604d719b26faf3178e40c82bf08c3
viiper94 <viiper94@gmail.com>
2019-10-09 14:08:05 +0300

    added translations

---
+++ resources/lang/en/navbar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+
+    'home' => 'Home',
+    'about' => 'About',
+    'artists' => 'Artists',
+    'studio' => 'Studio',
+    'ctschool' => 'CTSchool',
+    'reviews' => 'Reviews',
+    'demo' => 'Demo',
+    'contact' => 'Contact',
+
+];

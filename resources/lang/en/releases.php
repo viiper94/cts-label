a1694cd1b571f2ace8a896320fd3714d10433c50
viipe94 <viiper94@gmail.com>
2019-11-11 14:30:10 +0200

    moved some strings to other file

--- resources/lang/en/releases.php
+++ resources/lang/en/releases.php
@@ -1,14 +1,8 @@
 <?php
 return [
-
     'release_number' => 'REL.Number',
     'release_date' => 'REL.Date',
     'search_release' => 'Search Release',
     'tracklist' => 'Tracklist:',
     'related_releases' => 'Related releases:',
-
-    'ru' => 'RU',
-    'en' => 'EN',
-    'ua' => 'UA',
-
 ];

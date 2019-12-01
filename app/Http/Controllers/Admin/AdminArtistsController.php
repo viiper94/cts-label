c604a4f62cad82357fbda15d79aacc182f713c9b
Eugene <viiper94@gmail.com>
2019-12-01 15:02:01 +0200

    updated ck-editor

---
+++ app/Http/Controllers/Admin/AdminArtistsController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Artist;
+use Illuminate\Http\Request;
+
+class AdminArtistsController extends Controller{
+
+
+
+}

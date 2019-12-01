b782b48f3a0b892df0196ef27ff4f07d69432341
Eugene <viiper94@gmail.com>
2019-12-01 15:21:15 +0200

    added admin panel

--- app/Release.php
+++ app/Release.php
@@ -17,8 +17,25 @@ class Release extends Model{
         'visible' => 'boolean'
     ];
 
+    protected $guarded = [
+        'id',
+        'sort_id',
+        'image',
+        'release_date',
+        'visible',
+
+        'edit_release',
+        'search-by',
+        'related'
+    ];
+
     public function related(){
         return $this->belongsToMany('App\Release', 'related_releases', 'release_id', 'related_id');
+    }
+
+    public static function getLatestSortId(){
+        $release = Release::select('sort_id')->orderBy('sort_id', 'desc')->first()->toArray();
+        return $release['sort_id'];
     }
 
 }

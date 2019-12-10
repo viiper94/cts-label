4d050ada3ee309df91b2deb4b991e5d6c08a3a8a
Eugene <viiper94@gmail.com>
2019-12-10 12:28:22 +0200

    added artists sorting

--- app/Http/Controllers/Admin/AdminArtistsController.php
+++ app/Http/Controllers/Admin/AdminArtistsController.php
@@ -89,4 +89,46 @@ class AdminArtistsController extends Controller{
         }
     }
 
+    public function resort(Request $request){
+        foreach($request->post('sort') as $id => $sort){
+            $artist = Artist::find($id);
+            $artist->sort_id = $sort;
+            $artist->save();
+        }
+        return redirect()->back()->with(['success' => 'Артисты успешно отсортированы!']);
+    }
+
+    public function sortup(Request $request, $id){
+        if(isset($id)){
+            $artist = Artist::find($id);
+            $up_artist = Artist::where('sort_id', '>', $artist->sort_id)->orderBy('sort_id', 'asc')->first();
+            if(!$up_artist) return redirect()->back();
+            return $this->swapSort($artist, $up_artist)  ?
+                redirect()->back()->with(['success' => 'Артист успешно отредактирован!']) :
+                redirect()->back()->withErrors(['Возникла ошибка =(']);
+        }else{
+            return abort(404);
+        }
+    }
+
+    public function sortdown(Request $request, $id){
+        if(isset($id)){
+            $artist = Artist::find($id);
+            $down_artist = Artist::where('sort_id', '<', $artist->sort_id)->orderBy('sort_id', 'desc')->first();
+            if(!$down_artist) return redirect()->back();
+            return $this->swapSort($artist, $down_artist) ?
+                redirect()->back()->with(['success' => 'Артист успешно отредактирован!']) :
+                redirect()->back()->withErrors(['Возникла ошибка =(']);
+        }else{
+            return abort(404);
+        }
+    }
+
+    private function swapSort($artist, $slave_artist){
+        $tmp = $artist->sort_id;
+        $artist->sort_id = $slave_artist->sort_id;
+        $slave_artist->sort_id = $tmp;
+        return $artist->save() && $slave_artist->save();
+    }
+
 }

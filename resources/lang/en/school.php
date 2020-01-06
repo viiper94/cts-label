ddcc90f60ca7f83de5faa46bf7780af0d39307cb
Eugene <viiper94@gmail.com>
2020-01-06 12:33:22 +0200

    fixed translation

--- resources/lang/en/school.php
+++ resources/lang/en/school.php
@@ -6,7 +6,7 @@ return [
     'about_courses' => 'About courses',
     'equipment' => 'Equipment',
     'teachers' => 'Teachers',
-    'cost_of_education' => 'Cost of education',
+    'cost_of_education' => 'Prices',
     'contact' => 'Contact',
 
     'sound_producer' => 'Sound Producer',
@@ -26,29 +26,29 @@ return [
 
     'check_full_set' => 'Check Full Set on <br>"STUDIO" Section',
 
-    'about_text' => 'CTSchool courses are designed for PC users who have only a general idea of music creating and 
-                            studio sound engineering, as well as for those who want to improve their skills. 
-                            The focus of the CTSchool program is sound engineering and electronic music creation as well. 
-                            Experienced producers, musicians, established artists, sound engineers and DJs will 
+    'about_text' => 'CTSchool courses are designed for PC users who have only a general idea of music creating and
+                            studio sound engineering, as well as for those who want to improve their skills.
+                            The focus of the CTSchool program is sound engineering and electronic music creation as well.
+                            Experienced producers, musicians, established artists, sound engineers and DJs will
                             share their practical skills with you in the process of studying.',
-    'about_text_2' => 'Graduates of the school receive a certificate of CTSchool completion, a recommendation for 
-                            admission to the National Academy of Culture and Arts Managing Staff in the specialty: 
-                            sound engineer, sound engineer-arranger, sound engineer-DJ; a discount on the purchase of 
-                            equipment in the store Muztorg, Real Music and ART-R. Tracks of the best graduates will be 
-                            released on CTS Records. Successful students will receive recommendations for further 
+    'about_text_2' => 'Graduates of the school receive a certificate of CTSchool completion, a recommendation for
+                            admission to the National Academy of Culture and Arts Managing Staff in the specialty:
+                            sound engineer, sound engineer-arranger, sound engineer-DJ; a discount on the purchase of
+                            equipment in the store Muztorg, Real Music and ART-R. Tracks of the best graduates will be
+                            released on CTS Records. Successful students will receive recommendations for further
                             employment in the field of sound engineering.',
-    'about_text_3' => 'CTSchool works directly with manufacturers and suppliers of SOFT & HARDWARE. 
+    'about_text_3' => 'CTSchool works directly with manufacturers and suppliers of SOFT & HARDWARE.
                             All software in the above list is LICENSED.',
-    'about_text_4' => 'Only in CTSchool you have the opportunity to take a training course in working with electronic 
-                            dance music in PRO TOOLS HD 3 Accel System on MAC OS. CTSchool works closely with A&T Trade 
+    'about_text_4' => 'Only in CTSchool you have the opportunity to take a training course in working with electronic
+                            dance music in PRO TOOLS HD 3 Accel System on MAC OS. CTSchool works closely with A&T Trade
                             and MUZTORG, holds joint events, seminars, and master classes.',
     'about_text_5' => 'CTSchool recruits groups of up to 5 students, this allows to improve the education quality.',
-    'about_text_6' => 'Prices are for groups of 5 people. The cost of classes in mini-groups (2-3 people) depends on 
-                            the number of people in the group. The development of an individual course is possible. 
+    'about_text_6' => 'Prices are for groups of 5 people. The cost of classes in mini-groups (2-3 people) depends on
+                            the number of people in the group. The development of an individual course is possible.
                             One lesson - 2 academic hours.',
 
     'lessons_held' => 'Classes are held:',
-    'lessons_held_text' => 'at the CTS studio,<br>at the CINEMA club,<br>in the studio of the National Academy of 
+    'lessons_held_text' => 'at the CTS studio,<br>at the CINEMA club,<br>in the studio of the National Academy of
                                 Culture and Arts Managing Staff,<br>at the MUZTORG store.',
 
     'lesson_holds_sergio' => 'Lesson holds Sergio Mega',
@@ -61,8 +61,8 @@ return [
 
     'mail_body' => 'body=My name:%0AMy phone number:%0A&Subject=Education in CTSchool',
 
-    'contact_text_1' => 'You can contact us by leaving your message on the 
-                                <a href="https://www.facebook.com/groups/161704380617200" target="_blank" class="yellow-link">CTSchool Facebook page</a>, 
+    'contact_text_1' => 'You can contact us by leaving your message on the
+                                <a href="https://www.facebook.com/groups/161704380617200" target="_blank" class="yellow-link">CTSchool Facebook page</a>,
                                 right after we will invite you to the interview individually.',
     'contact_text_2' => 'APPLICATION FOR AN INTERVIEW IS ALSO POSSIBLE BY PHONE:',
     'contact_info' => '<p>+38 098 685 40 33</p><p>E-MAIL: <a href="mailto:info@cts-label.com">info@cts-label.com</a></p>',

1add891af08d90663dc1cbc95c9e948e29be4ff2
Eugene <viiper94@gmail.com>
2020-01-11 11:30:34 +0200

    added clipboard js

--- resources/views/admin/layout/scripts.blade.php
+++ resources/views/admin/layout/scripts.blade.php
@@ -1,4 +1,5 @@
 <script src="/assets/js/jquery-1.12.2.min.js"></script>
+<script src="/assets/js/clipboard.min.js"></script>
 @yield('scripts')
 <script src='/assets/js/jquery.sticky.js'></script>
 <script src="/assets/js/admin.js"></script>

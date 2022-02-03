e74c4b5a394bc7c0d14f17de310426a71156c713
Eugene <viiper94@gmail.com>
2022-02-03 13:08:28 +0200

    fixed feedback email template

--- resources/views/emails/feedback_result.blade.php
+++ resources/views/emails/feedback_result.blade.php
@@ -1,15 +1,15 @@
-{{ $result->name }} ({{ $result->email }}) оценил "{{ $feedback->feedback_title }}":
+<h3>{{ $result->name }} ({{ $result->email }}) оценил "{{ $feedback->feedback_title }}":</h3>
 
 
 @foreach($result->rates as $track => $score)
-{{ $score }} - "{{ $track }}"
+<p>{{ $score }} - "{{ $track }}"</p>
 @endforeach
 
 @if(count($result->rates) > 1)
-По его мнению, лучший микс в этом релизе - "{{ $result->best_track }}"
+<p>По его мнению, лучший микс в этом релизе - <b>{{ $result->best_track }}</b></p>
 @endif
 
-{{$result->name}}
-{{$result->email}}
-
+{{$result->name}}<br>
+{{$result->email}}<br>
+<br>
 {{$result->comment}}

5c3f1819028e6710ac11da2d3a1a7317c1bd9be9
Eugene <viiper94@gmail.com>
2022-09-30 15:34:43 +0300

    added emailing template for testing

--- resources/views/emails/emailing/ade.blade.php
+++ resources/views/emails/emailing/ade.blade.php
@@ -1,28 +1,32 @@
 @component('mail::message')
-# Dear {{ $name }}!
 
-Here is Olga – manager of Singer, DJ & Producer Ki Yera,
-I'm now compiling a schedule for Krystyna Yaroshenko aka Ki Yera.
+    # Dear {{ $name }}!
 
-Ki Yera would like to meet with you at ADE.
+    Here is Olga – manager of Singer, DJ & Producer Ki Yera,
+    I'm now compiling a schedule for Krystyna Yaroshenko aka Ki Yera.
 
-This year we are focused on:
+    Ki Yera would like to meet with you at ADE.
 
-- Ki Yera’s bookings. We are looking for booking agent/agency for arranging and handling gigs requests for Europe and Asia.
-- Presenting Ki Yera’s ADE 2019 sampler.
+    This year we are focused on:
 
-Ki Yera will stay in Amsterdam from Wednesday 16 till Friday 18 of October, please let me know 3 time slots suitable for you.
-She prefers to arrange her meetings in DeLaMar Theatre.
+    - Ki Yera’s bookings. We are looking for booking agent/agency for arranging and handling gigs requests for Europe and Asia.
+    - Presenting Ki Yera’s ADE 2019 sampler.
 
-Looking forward to hearing from you.
+    Ki Yera will stay in Amsterdam from Wednesday 16 till Friday 18 of October, please let me know 3 time slots suitable for you.
+    She prefers to arrange her meetings in DeLaMar Theatre.
 
-Kind regards,
+    Looking forward to hearing from you.
 
-Olga
-Ki Yera Management
-ADE mob. +380669899604
+    Kind regards,
 
-<djkiyera@gmail.com>
-[https://soundcloud.com/kiyeradj](https://soundcloud.com/kiyeradj)
+    Olga
+    Ki Yera Management
+    ADE mob. +380669899604
+
+    <djkiyera@gmail.com>
+
+    [https://soundcloud.com/kiyeradj](https://soundcloud.com/kiyeradj)
+
+    [unsubscribe]({{ route('unsubscribe', $hash) }})
 
 @endcomponent

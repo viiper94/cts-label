1ca4a55911b331e6de34757c280dbd2f440a33b9
Eugene <viiper94@gmail.com>
2022-10-01 17:04:05 +0300

    fixed missing attribute in fillable

--- app/EmailingContact.php
+++ app/EmailingContact.php
@@ -12,7 +12,7 @@ class EmailingContact extends Model{
 
     protected $table = 'email_contacts';
 
-    protected $fillable = ['name', 'email', 'company', 'position', 'additional', 'phone', 'website', 'country', 'company_foa'];
+    protected $fillable = ['name', 'full_name', 'email', 'company', 'position', 'additional', 'phone', 'website', 'country', 'company_foa'];
 
     public function channels(){
         return $this->belongsToMany('App\EmailingChannel', 'email_channels_contacts', 'contact_id', 'channel_id');

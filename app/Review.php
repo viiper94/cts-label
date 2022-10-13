95806e7c3817b2ae4b086f21e9d0202ebead36e2
Eugene <viiper94@gmail.com>
2022-10-13 10:46:00 +0300

    code optimizations

--- app/Review.php
+++ app/Review.php
@@ -8,21 +8,23 @@ class Review extends SharedModel{
         'data' => 'array'
     ];
 
+    public function __construct(){
+        $this->data = [
+            'reviews' => [0 => [
+                'author' => '',
+                'location' => '',
+                'review' => '',
+                'score' => 5,
+            ]],
+            'additional' => [0 => [
+                'author' => '',
+                'location' => '',
+            ]],
+        ];
+    }
+
     protected function asJson($value){
         return json_encode($value, JSON_UNESCAPED_UNICODE);
     }
-
-    public $defaultReview = [
-        'reviews' => [0 => [
-            'author' => '',
-            'location' => '',
-            'review' => '',
-            'score' => 5,
-        ]],
-        'additional' => [0 => [
-            'author' => '',
-            'location' => '',
-        ]],
-    ];
 
 }

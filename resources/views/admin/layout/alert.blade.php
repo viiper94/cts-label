4d37bcf0d5ac23ffa6ef6e601d49902643053446
Eugene <viiper94@gmail.com>
2022-10-27 11:30:24 +0300

    new alert as toast

--- resources/views/admin/layout/alert.blade.php
+++ resources/views/admin/layout/alert.blade.php
@@ -1,8 +1,23 @@
-<div class="container-fluid" >
-    @if(session()->get('success'))
-        <div class="alert alert-success" role="alert">{{ session()->get('success') }}</div>
-    @endif
-    @if($errors->any())
-        <div class="alert alert-danger" role="alert"><b>Ошибка:</b><br>{{ $errors->first() }}</div>
-    @endif
-</div>
+@if(session()->get('success'))
+    <div class="toast alert-toast text-bg-dark align-items-center position-absolute border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="99999999">
+        <div class="toast-header text-bg-success">
+            <strong class="me-auto">Успех!</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            {{ session()->get('success') }}
+        </div>
+    </div>
+@endif
+
+@if($errors->any())
+    <div class="toast alert-toast text-bg-dark align-items-center border-0 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true">
+        <div class="toast-header text-bg-danger">
+            <strong class="me-auto">Ошибка =(</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            {{ $errors->first() }}
+        </div>
+    </div>
+@endif

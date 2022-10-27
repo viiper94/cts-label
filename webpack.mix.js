a8cee461c637fa19cd7311d6436d29617c216e73
Eugene <viiper94@gmail.com>
2022-10-27 11:27:55 +0300

    adjust mix config

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js([
+        'resources/js/app.js',
+        'resources/js/admin/feedback.js',
+        'resources/js/admin/common.js',
+    ], 'public/js/admin.js')
+    // .sass('resources/sass/app.scss', 'public/css/public.css')
+    .sass('resources/sass/admin.scss', 'public/css/admin.css')
+    .version().disableNotifications();

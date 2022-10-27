67174cdaa002ce7c5793fbfb111cbf0418f59bde
Eugene <viiper94@gmail.com>
2022-10-27 11:31:58 +0300

    new bootstrap 5 search

--- resources/views/admin/layout/search.blade.php
+++ resources/views/admin/layout/search.blade.php
@@ -1,7 +1,7 @@
-<div class="navbar-right search">
-    <form class="navbar-form" role="search" enctype="application/x-www-form-urlencoded">
-        <div class="form-group">
-            <input type="text" class="form-control form-control__dark" placeholder="Search" name="q" value="{{ Request::input('q') }}">
-        </div>
-    </form>
-</div>
+<form role="search" enctype="application/x-www-form-urlencoded">
+    <div class="form-floating">
+        <input type="text" name="q" class="form-control text-bg-dark" id="search" placeholder="Поиск" value="{{ Request::input('q') }}">
+        <label for="floatingInput">Поиск</label>
+    </div>
+</form>
+<hr>

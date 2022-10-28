521d8c7db3a6371a499076055169655a37244043
Eugene <viiper94@gmail.com>
2022-10-28 21:19:35 +0300

    fixed toast bg and delay

--- resources/views/admin/layout/alert.blade.php
+++ resources/views/admin/layout/alert.blade.php
@@ -1,5 +1,5 @@
 @if(session()->get('success'))
-    <div class="toast alert-toast text-bg-dark align-items-center position-absolute border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="99999999">
+    <div class="toast alert-toast align-items-center position-absolute border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
         <div class="toast-header text-bg-success">
             <strong class="me-auto">Успех!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>

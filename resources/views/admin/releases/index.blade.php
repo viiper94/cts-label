9c7476ea7e028f1907f2d6b062fa77df2bfbaacd
Eugene <viiper94@gmail.com>
2022-10-28 12:20:22 +0300

    updated releases page layout

--- resources/views/admin/releases/index.blade.php
+++ resources/views/admin/releases/index.blade.php
@@ -2,7 +2,7 @@
 
 @section('admin-content')
 
-    <div class="container-fluid">
+    <div class="container-fluid admin-releases">
 
         @include('admin.layout.alert')
         <form id='sort_form' method='POST' action="{{ route('releases.resort') }}">
@@ -22,27 +22,44 @@
         <div class="row">
             @foreach($releases as $release)
                 <div class="col-md-4 col-sm-6 col-xs-12">
-                    <div class="card mb-3">
+                    <div class="card text-bg-dark mb-3">
                         <div class="row g-0">
-                            <div class="col-md-4">
-                                <img src="/images/releases/{{ $release->image ?? 'default.png' }}" class="img-fluid" alt="{{ $release->title }}">
-                            </div>
-                            <div class="col-md-8">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $release->title }}</h5>
-                                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                            <div class="col-12 d-flex g-0">
+                                <div class="card-img col-auto">
+                                    <img src="/images/releases/{{ $release->image ?? 'default.png' }}" class="img-fluid" alt="{{ $release->title }}">
                                 </div>
-                                <div class="card-footer">
-                                    <a class="btn btn-success" href="{{ route('releases.edit', $release->id) }}">
+                                <div class="card-info d-flex flex-column col">
+                                    <div class="card-header">
+                                        <h5 class="card-title text-nowrap">{{ $release->title }}</h5>
+                                    </div>
+                                    <div class="d-flex flex-grow-1">
+                                        <div class="card-body">
+                                            <p class="card-text">{{ $release->release_number }}</p>
+                                            <p class="card-text">{{ $release->release_date?->isoFormat('LL') }}</p>
+                                        </div>
+                                        <div class="card-sort d-flex flex-column">
+                                            <a href="{{ route('releases.sort', ['release' => $release->id, 'dir' => 'up']) }}" class="fs-2">
+                                                <i class="fa-solid fa-chevron-up"></i>
+                                            </a>
+                                            <input type="number" class="text-bg-dark" name="sort[{{ $release->id }}]" value="{{ $release->sort_id }}" form="sort_form">
+                                            <a href="{{ route('releases.sort', ['release' => $release->id, 'dir' => 'down']) }}" class="fs-2">
+                                                <i class="fa-solid fa-chevron-down"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 g-0">
+                                <div class="card-footer justify-content-end">
+                                    <a class="btn btn-sm btn-success" href="{{ route('releases.edit', $release->id) }}">
                                         <i class="fa-solid fa-pen me-2"></i>Редактировать
                                     </a>
                                     @if($release->feedback)
-                                        <a class="btn btn-info" href="{{ route('feedback_admin') }}/edit/{{ $release->feedback->slug }}">
+                                        <a class="btn btn-sm btn-info" href="{{ route('feedback_admin') }}/edit/{{ $release->feedback->slug }}">
                                             <i class="fa-solid fa-plus me-2"></i>Edit Feedback
                                         </a>
                                     @else
-                                        <a class="btn btn-info" href="{{ route('feedback_admin') }}/add/{{ $release->id }}">
+                                        <a class="btn btn-sm btn-info" href="{{ route('feedback_admin') }}/add/{{ $release->id }}">
                                             <i class="fa-solid fa-plus me-2"></i>New Feedback
                                         </a>
                                     @endif

dfff763d60ac6c8f8aa99c51c451e600d5e6a257
Eugene <viiper94@gmail.com>
2022-10-28 12:18:27 +0300

    removed axios

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,5 @@
 window.$ = window.jQuery = require('jquery');
 
-window.axios = require('axios');
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
-
 import "bootstrap/dist/js/bootstrap.min";
 import "floating-scroll/dist/jquery.floatingscroll.min"
 import "jquery-ui/dist/jquery-ui.min"

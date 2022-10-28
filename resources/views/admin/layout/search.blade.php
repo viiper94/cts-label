9561dcace334d59a2f5c8e264d703d41b68c200d
Eugene <viiper94@gmail.com>
2022-10-28 21:20:05 +0300

    search field fixes

--- resources/views/admin/layout/search.blade.php
+++ resources/views/admin/layout/search.blade.php
@@ -1,7 +1,4 @@
 <form role="search" enctype="application/x-www-form-urlencoded">
-    <div class="form-floating">
-        <input type="text" name="q" class="form-control text-bg-dark" id="search" placeholder="Поиск" value="{{ Request::input('q') }}">
-        <label for="floatingInput">Поиск</label>
-    </div>
+    <input type="text" name="q" class="form-control form-dark" id="search" placeholder="Поиск" value="{{ Request::input('q') }}">
 </form>
 <hr>

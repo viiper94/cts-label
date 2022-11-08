b2139b3cd3e43db92f27697840fe7ff87e6b2174
Eugene <viiper94@gmail.com>
2022-11-08 21:21:36 +0200

    added litepicker

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,3 +3,4 @@ window.$ = window.jQuery = require('jquery');
 import "bootstrap/dist/js/bootstrap.min";
 import "floating-scroll/dist/jquery.floatingscroll.min"
 import "jquery-ui/dist/jquery-ui.min"
+import Litepicker from "litepicker";

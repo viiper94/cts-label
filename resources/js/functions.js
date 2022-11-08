62005344e4b31593a882c114222333cc84e216d0
Eugene <viiper94@gmail.com>
2022-11-08 10:14:36 +0200

    added functions js file

---
+++ resources/js/functions.js
@@ -0,0 +1,10 @@
+function readURL(input, selector){
+    if(selector === undefined) selector = '#preview';
+    if (input.files && input.files[0]){
+        let reader = new FileReader();
+        reader.onload = function (e) {
+            $(selector).attr('src', e.target.result);
+        };
+        reader.readAsDataURL(input.files[0]);
+    }
+}

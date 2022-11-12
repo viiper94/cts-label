6ca9e931e98b0254b2315eccc399418b6bde1234
Eugene <viiper94@gmail.com>
2022-11-12 19:52:35 +0200

    fixed studio page layout

--- resources/views/admin/studio.blade.php
+++ resources/views/admin/studio.blade.php
@@ -2,68 +2,62 @@
 
 @section('assets')
     <link rel="stylesheet" href="/assets/css/jquery.ui.sortable.min.css">
-@endsection
-
-@section('scripts')
     <script src="/assets/js/jquery.ui.sortable.min.js"></script>
 @endsection
 
 @section('admin-content')
 
-    <div class="container">
-        @include('admin.layout.alert')
-        <div class="releases-actions">
-            <button class='btn btn-success add-service' data-action="{{ route('studio.store') }}">
-                <span class='glyphicon glyphicon-plus' aria-hidden='true'></span>
-                Новая услуга
+    <div class="container-fluid">
+        <div class="releases-actions sticky-top my-3">
+            <button data-action="{{ route('studio.store') }}" class="btn btn-primary add-service">
+                <i class="fa-solid fa-plus me-2"></i>Новая услуга
             </button>
         </div>
         @foreach($service_list as $services)
-            <h4>({{ $services[0]->lang }}) {{ \Illuminate\Support\Facades\Lang::choice('studio.services', 8, locale: $services[0]->lang) }}</h4>
-            <section class="panel panel__dark service-lang">
-                <div class="panel-body sortable">
+            <div class="card text-bg-dark service-lang mb-5">
+                <h4 class="card-header">({{ $services[0]->lang }}) {{ \Illuminate\Support\Facades\Lang::choice('studio.services', 8, locale: $services[0]->lang) }}</h4>
+                <div class="card-body sortable">
                     @foreach($services as $service)
-                        <img src="/images/studio/services/{{ $service->image }}" alt="{{ $service->service_alt }}" class="service-img"
+                        <img src="/images/studio/services/{{ $service->image }}" alt="{{ $service->service_alt }}" class="service-img p-3"
                              data-id="{{ $service->id }}" data-lang="{{ $service->lang }}" data-name="{{ $service->name }}"
                              data-visible="{{ $service->visible }}" data-action="{{ route('studio.update', $service->id) }}">
                     @endforeach
                 </div>
-            </section>
+            </div>
         @endforeach
 
         <div class="modal fade" id="serviceModal">
             <div class="modal-dialog modal-lg">
-                <div class="modal-content modal-content__dark">
+                <div class="modal-content">
                     <form action="{{ route('studio.store') }}" enctype="multipart/form-data" method="post" id="modal-form">
-                        <div class="modal-body">
+                        <div class="modal-body py-3">
                             @csrf
                             <div class="row">
                                 <div class="col-md-5">
-                                    <img src="/images/studio/services/default.png" id="preview">
-                                    <input type="file" name="image" id="uploader" accept="image/jpeg, image/png">
+                                    <img src="/images/studio/services/default.png" id="preview" class="img-fluid">
+                                    <input type="file" name="image" id="uploader" class="form-control form-dark" accept="image/jpeg, image/png">
                                 </div>
                                 <div class="col-md-7">
-                                    <div class="form-group">
-                                        <label>Язык</label><br>
-                                        <select class="form-control form-control__dark" name="lang" id="lang" required>
+                                    <div class="form-check mb-3">
+                                        <input type="hidden" name="visible" value="0">
+                                        <input type="checkbox" name="visible" id="visible" class="form-check-input">
+                                        <label for="visible" class="form-check-label">Опубликовано</label>
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label class="form-label form-dark">Название услуги</label>
+                                        <input type="text" class="form-control form-dark" name="name" required>
+                                    </div>
+                                    <div class="form-group mb-3">
+                                        <label class="form-label">Язык</label>
+                                        <select class="form-select form-dark" name="lang" id="lang" required size="3">
                                             <option value="en">English</option>
                                             <option value="ru">Русский</option>
                                             <option value="ua">Українська</option>
                                         </select>
                                     </div>
                                     <div class="form-group">
-                                        <label>Название услуги</label><br>
-                                        <input type="text" class="form-control form-control__dark" name="name" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <label>Ключевые слова</label><br>
-                                        <textarea name="service_alt" id="service_alt" rows="3" class="form-control form-control__dark"></textarea>
-                                    </div>
-                                    <div class="checkbox">
-                                        <label>
-                                            <input type="hidden" name="visible" value="0">
-                                            <input type="checkbox" name="visible" id="visible"> Опубликовано
-                                        </label>
+                                        <label class="form-label">Описание</label>
+                                        <textarea name="service_alt" id="service_alt" rows="3" class="form-control form-dark"></textarea>
                                     </div>
                                 </div>
                             </div>
@@ -73,10 +67,10 @@
                         <form method="post" id="delete-form" style="display:none;">
                             @csrf
                             @method('DELETE')
-                            <button type="submit" class="btn btn-danger" form="delete-form" onclick='return confirm("Удалить?")'><i class="fa-solid fa-trash"></i> Удалить</button>
+                            <button type="submit" class="btn btn-outline-danger" form="delete-form" onclick='return confirm("Удалить?")'><i class="fa-solid fa-trash"></i> Удалить</button>
                         </form>
-                        <button type="submit" class="btn btn-primary" form="modal-form"><i class="fa-solid fa-check"></i> Сохранить</button>
-                        <button type="button" class="btn btn-default" data-dismiss="modal"><i class="fa-solid fa-xmark"></i> Закрыть</button>
+                        <button type="submit" class="btn btn-primary" form="modal-form"><i class="fa-solid fa-floppy-disk"></i> Сохранить</button>
+                        <button type="button" class="btn btn-outline" data-bs-dismiss="modal"><i class="fa-solid fa-xmark"></i> Закрыть</button>
                     </div>
                 </div>
             </div>

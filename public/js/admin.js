e022227d9eec59a5770f983d6efb202861605863
Eugene <viiper94@gmail.com>
2022-11-21 19:38:02 +0200

    compiled assets

--- public/js/admin.js
+++ public/js/admin.js
@@ -3458,6 +3458,60 @@ $(document).ready(function () {
 
 /***/ }),
 
+/***/ "./resources/js/admin/studio.js":
+/*!**************************************!*\
+  !*** ./resources/js/admin/studio.js ***!
+  \**************************************/
+/***/ (() => {
+
+// Studio School
+$(document).ready(function () {
+  $('.service-lang .sortable').sortable({
+    stop: function stop(event, ui) {
+      var data = {};
+      $.map($(this).find('img'), function (el) {
+        data[$(el).index()] = $(el).data('id');
+      });
+      $.ajax({
+        url: '/cts-admin/studio/resort',
+        data: {
+          'data': data
+        },
+        beforeSend: function beforeSend() {
+          $('.service-lang .msg').html('Сортировка...');
+        },
+        success: function success(response) {
+          $('.service-lang .msg').html('Пересортировано');
+        }
+      });
+    }
+  });
+  $('.add-service').click(function () {
+    $('#serviceModal').find('#preview').attr('src', '/images/studio/services/default.png');
+    $('#serviceModal').find('[name=name]').val('');
+    $('#serviceModal').find('#service_alt').val('');
+    $('#serviceModal').find('#visible').prop('checked', false);
+    $('#serviceModal').find('#lang option[value=en]').prop('selected', true);
+    $('#serviceModal').find('#modal-form').attr('action', $(this).data('action'));
+    $('#serviceModal #delete-form').hide();
+    if ($('#serviceModal #modal-form [name=_method]').length > 0) $('#serviceModal #modal-form [name=_method]').remove();
+    $('#serviceModal').modal('show');
+  });
+  $('.service-lang .service-img').click(function () {
+    $('#serviceModal').find('#preview').attr('src', $(this).attr('src'));
+    $('#serviceModal').find('[name=name]').val($(this).data('name'));
+    $('#serviceModal').find('#service_alt').val($(this).attr('alt'));
+    $('#serviceModal').find('#visible').prop('checked', $(this).data('visible') === '1');
+    $('#serviceModal').find('#lang option[value=' + $(this).data('lang') + ']').prop('selected', true);
+    $('#serviceModal').find('#modal-form').attr('action', $(this).data('action'));
+    $('#serviceModal #delete-form').css('display', 'inline-block').attr('action', $(this).data('action'));
+    if ($('#serviceModal #modal-form [name=_method]').length === 0) $('#serviceModal #modal-form').append('<input type="hidden" name="_method" value="PUT">');
+    $('#serviceModal').modal('show');
+  });
+});
+
+/***/ }),
+
 /***/ "./resources/js/app.js":
 /*!*****************************!*\
   !*** ./resources/js/app.js ***!
@@ -3486,6 +3540,7 @@ __webpack_require__(/*! ./admin/common.js */ "./resources/js/admin/common.js");
 __webpack_require__(/*! ./admin/feedback.js */ "./resources/js/admin/feedback.js");
 __webpack_require__(/*! ./admin/releases.js */ "./resources/js/admin/releases.js");
 __webpack_require__(/*! ./admin/reviews.js */ "./resources/js/admin/reviews.js");
+__webpack_require__(/*! ./admin/studio.js */ "./resources/js/admin/studio.js");
 
 /***/ }),
 

3a5d711843b8a37704dca0b4eac547995319627b
Eugene <viiper94@gmail.com>
2022-11-21 15:56:11 +0200

    added bootstrap star rating

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,8 @@ window.$ = window.jQuery = require('jquery');
 import "bootstrap/dist/js/bootstrap.min";
 import "floating-scroll/dist/jquery.floatingscroll.min"
 import "jquery-ui/dist/jquery-ui.min"
-import Litepicker from "litepicker";
+import Litepicker from "litepicker"
+import "bootstrap-star-rating"
 
 require('./admin/common.js');
 require('./admin/feedback.js');

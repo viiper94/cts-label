0d609eab67f0c64c1d815b23cf85969b2e55e85e
Eugene <viiper94@gmail.com>
2022-11-22 11:25:39 +0200

    removed old scripts file

--- resources/views/admin/layout/scripts.blade.php
+++ resources/views/admin/layout/scripts.blade.php
@@ -1,3 +1,3 @@
 <script type="text/javascript" src="{{ mix('js/admin.js') }}"></script>
 @yield('scripts')
-<script src="/assets/js/admin.js?1110"></script>
+{{--<script src="/assets/js/admin.js?1110"></script>--}}

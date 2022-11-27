d0062a206562e061f4e16f842dd30d7aa3cf2283
Eugene <viiper94@gmail.com>
2022-11-27 15:00:04 +0200

    added feedback pages scripts

--- resources/js/admin/feedback.js
+++ resources/js/admin/feedback.js
@@ -10,4 +10,17 @@ $(document).ready(function(){
         e.clearSelection();
     });
 
+    $('.related_last_five').click(function(e){
+        e.preventDefault();
+        $('.related-all-feedback input').prop('checked', false);
+        for(let i = 0; i < 5; i++){
+            $($('.related-all-feedback input')[i]).prop('checked', true);
+        }
+    });
+
+    $('.deselect-btn').click(function(e){
+        e.preventDefault();
+        $('.related-all-feedback input').prop('checked', false);
+    });
+
 });

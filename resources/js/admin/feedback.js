82151ee6246d981defe1574d4105b8c0f670ae49
Eugene <viiper94@gmail.com>
2022-11-30 10:13:06 +0200

    removed clipboard function

--- resources/js/admin/feedback.js
+++ resources/js/admin/feedback.js
@@ -1,14 +1,5 @@
-import ClipboardJS from "clipboard";
-
+// Feedback
 $(document).ready(function(){
-
-    let clipboard = new ClipboardJS('.copy-link');
-    clipboard.on('success', function(e) {
-        $(e.trigger).html('<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>\n' +
-            '                                <span class="hidden-xs hidden-sm">Скопировано!</span>');
-
-        e.clearSelection();
-    });
 
     $('.related_last_five').click(function(e){
         e.preventDefault();

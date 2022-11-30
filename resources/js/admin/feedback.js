24daee28699c9c95af39ca4ae93218ada3b67ae3
Eugene <viiper94@gmail.com>
2022-11-30 10:13:30 +0200

    added function to ajax add track template

--- resources/js/admin/feedback.js
+++ resources/js/admin/feedback.js
@@ -14,4 +14,25 @@ $(document).ready(function(){
         $('.related-all-feedback input').prop('checked', false);
     });
 
+    $('.add-track-btn').click(function(){
+        let $button = $(this);
+        let index = $button.data('index');
+        $.ajax({
+            data: {
+                index: index,
+            },
+            url : '/cts-admin/feedback/template',
+            success: function(response){
+                $button.data('index', index+1);
+                $('.tracks').append(response.html);
+            }
+        });
+    });
+
+    $(document).on('click','.delete-track-btn', function(){
+        if(confirm('Удалить?')){
+            $(this).parents('.track-card').remove();
+        }
+    });
+
 });

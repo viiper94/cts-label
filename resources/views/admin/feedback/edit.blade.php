7814b88cb94d47a45019482efb1d03acf35882b6
Eugene <viiper94@gmail.com>
2022-11-30 10:14:19 +0200

    feedback edit page layout fixes

--- resources/views/admin/feedback/edit.blade.php
+++ resources/views/admin/feedback/edit.blade.php
@@ -19,7 +19,7 @@
                 <i class="fa-solid fa-arrow-up-right-from-square me-2"></i>Релиз на сайте
             </a>
         @endif
-        <form enctype="multipart/form-data" method="post" id="edit_release"
+        <form enctype="multipart/form-data" method="post" id="edit_release" class="mb-3"
               action="{{ $feedback->id ? route('feedback.update', $feedback->id) : route('feedback.store', $feedback->release?->id) }}">
             @csrf
             @if($feedback->id)
@@ -38,17 +38,17 @@
                     </div>
                 </div>
                 <div class="col-md col-xs-12">
-                    <div class="form-check mb-3">
-                        <input type="hidden" name="visible" value="0">
-                        <input type="checkbox" name="visible" id="visible" class="form-check-input" @checked($feedback->visible)>
-                        <label for="visible" class="form-check-label">Опубликовано</label>
-                    </div>
                     <div class="form-group mb-3">
                         <label for="title" class="form-label">Название</label>
                         <input type="text" class="form-control form-dark form-control-lg" name="feedback_title" id="title" value="{{ old('feedback_title') ?? $feedback->feedback_title }}" required>
                         @error('feedback_title')
                             <p class="help-block">{{ $message }}</p>
                         @enderror
+                    </div>
+                    <div class="form-check mb-3">
+                        <input type="hidden" name="visible" value="0">
+                        <input type="checkbox" name="visible" id="visible" class="form-check-input" @checked($feedback->visible)>
+                        <label for="visible" class="form-check-label">Опубликовано</label>
                     </div>
                     <div class="card text-bg-dark related-all-feedback">
                         <div class="card-header d-flex align-items-center justify-content-between">
@@ -76,68 +76,64 @@
                     </div>
                 </div>
             </div>
-            <div class="col-xs-12" id="reviews">
-                @foreach($feedback->tracks as $key => $track)
-                    <div class="review" id="feedback-{{ $key }}">
-                        <a class="delete-review-btn btn"><span class="glyphicon glyphicon-remove"></span></a>
-                        <div class="form-group">
-                            <label>Название трека</label>
-                            <input type="text" class="form-control form-dark" name="tracks[{{ $key }}][title]" value="{{ $track['title'] }}" required>
-                        </div>
-                        <div class="form-group col-xs-6">
-                            <label>Файл в высоком качестве</label><br>
-                            <input type="file" style="font-size: 13px;" name="tracks[{{ $key }}][320]" data-id="{{ $key }}" accept=".mp3">
-                        </div>
-                        <div class="form-group col-xs-6">
-                            <label>Файл в низком качестве</label><br>
-                            <input type="file" style="font-size: 13px;" name="tracks[{{ $key }}][96]" data-id="{{ $key }}" accept=".mp3">
-                        </div>
-                        @if(key_exists(96, $track) && $track[96] !== '' || key_exists(320, $track) && $track[320] !== '')
-                            <audio src="/audio/feedback/{{ $feedback->slug }}/{{ $feedback->LQDir() }}/{{ $track[$feedback->LQDir()] }}" controls style="width: 100%;"></audio>
-                        @endif
-                        <div class="clearfix"></div>
-                    </div>
-                @endforeach
-            </div>
-            <div class="col-xs-12">
-                <a class="add-review-btn btn btn-info" data-index="{{ $f_index ?? 0 }}" data-target="reviews"><span class="glyphicon glyphicon-plus"></span> Добавить</a>
-                @if($feedback->archive_name && file_exists(public_path('audio/feedback/').$feedback->slug.'/'.$feedback->archive_name))
-                    <a href="/audio/feedback/{{ $feedback->slug }}/{{ $feedback->archive_name }}" class="btn btn-success">
-                        <span class='glyphicon glyphicon-download-alt' aria-hidden='true'></span>
-                        Скачать архив
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <h3 class="mb-0">Треки</h3>
+                @if($feedback->hasArchive())
+                    <a href="/audio/feedback/{{ $feedback->slug }}/{{ $feedback->archive_name }}" class="btn btn-outline btn-sm">
+                        <i class="fa-solid fa-file-zipper me-2"></i>Скачать архив
                     </a>
                 @endif
             </div>
-            <div class="clearfix"></div>
+            <div class="row g-0">
+                <div class="tracks col-12">
+                    @foreach($feedback->tracks as $key => $track)
+                        @include('admin.feedback.track_item', compact('key', 'track'))
+                    @endforeach
+                </div>
+                <div class="col-12">
+                    <a class="add-track-btn btn btn-outline" data-index="{{ count($feedback->tracks) }}">
+                        <i class="fa-solid fa-plus me-2"></i>Добавить трек
+                    </a>
+                </div>
+
+            </div>
         </form>
 
         @if($feedback->results->count() > 0)
-            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+            <div class="d-flex justify-content-between align-items-center mb-3">
+                <h3 class="mb-0">Фидбеки</h3>
+            </div>
+            <div class="accordion mb-3" id="accordion">
                 @foreach($feedback->results as $key => $result)
-                    <div class="panel panel__dark panel-default panel-default__dark">
-                        <a class="delete-feedback-result-btn btn" href="/cts-admin/feedback/removeResult/{{ $result->id }}"
-                            onclick="return confirm('Вы уверены, что хотите удалить?')">
-                            <span class="glyphicon glyphicon-remove"></span>
-                        </a>
-                        <div class="panel-heading panel-heading__dark" role="tab" id="heading_{{ $key }}">
-                            <h4 class="panel-title">
-                                <a role="button" data-toggle="collapse" data-parent="#accordion" href="#collapse_{{ $key }}" aria-expanded="false" aria-controls="collapse_{{ $key }}">
-                                    Фидбэк от {{ $result->name }} ({{ $result->created_at }})
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse_{{ $key }}" class="panel-collapse collapse in" role="tabpanel" aria-labelledby="heading_{{ $key }}">
-                            <div class="panel-body panel-body__dark">
-                                 E-mail: <b>{{ $result->email }}</b><br><br>
-                                <b>Оценки</b>:<br>
-                                @foreach($result->rates as $track => $score)
-                                    <b>{{ $score }}</b>: "{{ $track }}"<br>
-                                @endforeach
-                                <br>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header" id="heading_{{ $key }}">
+                            <button class="accordion-button text-bg-dark" type="button" data-bs-toggle="collapse"
+                                    data-bs-target="#collapse_{{ $key }}" aria-expanded="true" aria-controls="collapse_{{ $key }}">
+                                <b>{{ $result->name }}</b>&nbsp;<i>({{ $result->created_at }})</i>
+                            </button>
+                        </h2>
+                        <div id="collapse_{{ $key }}" class="accordion-collapse collapse" aria-labelledby="heading_{{ $key }}" data-bs-parent="#accordion">
+                            <div class="accordion-body text-bg-dark">
+                                <p><i class="fa-solid fa-envelope me-2"></i>E-mail: <b>{{ $result->email }}</b></p>
+                                <p class="mb-0"><i class="fa-solid fa-star me-2"></i><b>Оценки</b>:</p>
+                                <ul style="list-style: none" class="mb-3">
+                                    @foreach($result->rates as $track => $score)
+                                        <li><b>{{ $score }}</b>: "{{ $track }}"</li>
+                                    @endforeach
+                                </ul>
                                 @if($result->best_track)
-                                    Best track: <b>{{ $result->best_track }}</b><br>
+                                    <p><i class="fa-solid fa-heart me-2"></i>Best track: <b>{{ $result->best_track }}</b></p>
                                 @endif
-                                Коммент: <b>{!! nl2br(e($result->comment)) !!}</b>
+                                <p class="mb-0"><i class="fa-solid fa-comment me-2"></i>Коммент:</p>
+                                <p><b>{!! nl2br(e($result->comment)) !!}</b></p>
+                                <form action="{{ route('feedback.deleteResult', $result->id) }}" method="post" class="mt-3">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="delete-feedback-result-btn btn btn-outline-danger btn-sm"
+                                       onclick="return confirm('Вы уверены, что хотите удалить?')">
+                                        <i class="fa-solid fa-trash me-2"></i>Удалить
+                                    </button>
+                                </form>
                             </div>
                         </div>
                     </div>

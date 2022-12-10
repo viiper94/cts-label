e3d4000ea89dc9c2c400d3cf20272f9a106f199b
Eugene <viiper94@gmail.com>
2022-12-10 10:05:39 +0200

    renamed script file

---
+++ resources/js/admin.js
@@ -0,0 +1,13 @@
+window.$ = window.jQuery = require('jquery');
+
+import "bootstrap/dist/js/bootstrap.min";
+import "floating-scroll/dist/jquery.floatingscroll.min"
+import "jquery-ui/dist/jquery-ui.min"
+import Litepicker from "litepicker"
+import "bootstrap-star-rating"
+
+require('./admin/common.js');
+require('./admin/feedback.js');
+require('./admin/releases.js');
+require('./admin/reviews.js');
+require('./admin/studio_school.js');

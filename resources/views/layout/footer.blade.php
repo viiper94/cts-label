5df9454e7d4a3c4d66aaf145c3a7e407f5e78463
Eugene <viiper94@gmail.com>
2022-12-18 12:51:51 +0200

    fixed footer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,55 +1,42 @@
 <footer>
     <div class="container">
-        <div class="col-xs-12">
-            <div class="row">
-                <div class="col-xs-12 col-md-6 col-sm-7">
-                    <div class="row">
-                        <h4>@lang('footer.partners')</h4>
-                        <div class="partners">
-                            <a href="http://nightlife.tochka.net/besttrack/" target="_blank">
-                                <img src="/assets/img/best-track.png" height="40" />
-                            </a>
-                            <a href="http://nooguardia.com/noo.htm" target="_blank">
-                                <img src="/assets/img/heads.png" height="40" />
-                                <a href="http://www.kissfm.ua/" target="_blank">
-                                    <img src="/assets/img/kissfm.png"  height="40" />
-                                </a>
-                                <a href="#" target="_blank">
-                                    <img src="/assets/img/sweet.png" height="40" />
-                                </a>
-                                <a href="https://www.facebook.com/MARTS.art.project/" target="_blank">
-                                    <img src="/assets/img/marls.png" height="40" />
-                                </a>
-                                <a href="http://nightlife.tochka.net/" target="_blank">
-                                    <img src="/assets/img/tochka.png" height="40" />
-                                </a>
-                            </a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-xs-12 col-md-6 col-sm-5  text-right footer-socials">
-                    <h4>@lang('footer.follow_us')</h4>
-                    <div>
-                        <a href="https://www.beatport.com/label/cts-creative-technologies-studio/4470" class="socials socials-beatport" target="_blank"></a>
-                        <a class="socials socials-itunes" target="_blank"></a>
-                        <a href="https://www.facebook.com/CTS.Records/" class="socials socials-facebook" target="_blank"></a>
-                        <a href="https://www.youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" class="socials socials-youtube" target="_blank"></a>
-                        <a href="https://www.residentadvisor.net/record-label.aspx?id=2317" class="socials socials-ra" target="_blank"></a>
-                        <a href="https://new.vk.com/ctschool" class="socials socials-vk" target="_blank"></a>
-                        <a href="https://soundcloud.com/cts-records" class="socials socials-soundcloud" target="_blank"></a>
-                        <a href="http://www.last.fm/ru/music/CTS+Records" class="socials socials-as" target="_blank"></a>
-                        <a class="socials socials-rss" target="_blank"></a>
-                        <a href="https://twitter.com/CTS_RECORDS" class="socials socials-twitter" target="_blank"></a>
-                        <a class="socials socials-instagram" target="_blank"></a>
-                        <a href="https://www.discogs.com/label/74862-CTS-records" class="socials socials-discogs" target="_blank"></a>
-                        <a class="socials socials-spotify" target="_blank"></a>
-                        <a href="https://myspace.com/cts_records" class="socials socials-myspace" target="_blank"></a>
-                    </div>
+        <div class="row">
+            <div class="col-xs-12 col-md-6 col-sm-7">
+                <p>@lang('footer.partners')</p>
+                <div class="partners">
+                    <img src="/assets/img/best-track.png">
+                    <img src="/assets/img/heads.png">
+                    <a href="https://kissfm.ua/" target="_blank">
+                        <img src="/assets/img/kissfm.png">
+                    </a>
+                    <img src="/assets/img/sweet.png">
+                    <a href="https://facebook.com/MARTS.art.project/" target="_blank">
+                        <img src="/assets/img/marls.png">
+                    </a>
+                    <img src="/assets/img/tochka.png">
                 </div>
             </div>
-            <div class="row">
-                <h3>&copy; CTS RECORDS</h3>
+            <div class="col-xs-12 col-md-6 col-sm-5">
+                <p class="text-end">@lang('footer.follow_us')</p>
+                <div class="text-end">
+                    <a href="https://beatport.com/label/cts-creative-technologies-studio/4470" class="socials socials-beatport" target="_blank"></a>
+                    <button type="button" class="socials socials-itunes"></button>
+                    <a href="https://facebook.com/CTS.Records/" class="socials socials-facebook" target="_blank"></a>
+                    <a href="https://youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" class="socials socials-youtube" target="_blank"></a>
+                    <a href="https://ra.co/labels/2317" class="socials socials-ra" target="_blank"></a>
+                    <a href="https://soundcloud.com/cts-records" class="socials socials-soundcloud" target="_blank"></a>
+                    <a href="https://last.fm/music/CTS+Records" class="socials socials-as" target="_blank"></a>
+                    <button type="button" class="socials socials-rss"></button>
+                    <a href="https://twitter.com/CTS_RECORDS" class="socials socials-twitter" target="_blank"></a>
+                    <button type="button" class="socials socials-instagram"></button>
+                    <a href="https://discogs.com/label/74862-CTS-records" class="socials socials-discogs" target="_blank"></a>
+                    <a href="https://open.spotify.com/user/892tia21rho36ip7xqygpqxpq" class="socials socials-spotify" target="_blank"></a>
+                    <a href="https://myspace.com/cts_records" class="socials socials-myspace" target="_blank"></a>
+                </div>
             </div>
+        </div>
+        <div class="row my-4">
+            <p class="col-12 copyright">&copy; CTS RECORDS</p>
         </div>
     </div>
 </footer>

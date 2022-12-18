e7e813802af6ebafae1c2dbdf29a104862d6a065
Eugene <viiper94@gmail.com>
2022-12-18 14:42:11 +0200

    fixed width

--- resources/views/layout/youtube.blade.php
+++ resources/views/layout/youtube.blade.php
@@ -1,6 +1,6 @@
 <div class="widget">
     <h3><a href="https://www.youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA">YOUTUBE</a></h3>
     <div class="widget-content">
-        <iframe width="100%" height="210" src="https://www.youtube.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen></iframe>
+        <iframe width="300" height="210" src="https://www.youtube.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen></iframe>
     </div>
 </div>

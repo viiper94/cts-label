8fee9ec748ccbde7d6f288200bc5f10eaa58ca3b
Eugene <viiper94@gmail.com>
2022-12-18 10:38:26 +0200

    removed fixed scripts

--- public/assets/js/scripts.js
+++ public/assets/js/scripts.js
@@ -1,29 +1,12 @@
 $(document).ready(function(){
 
-    var hash = window.location.hash;
-    if ($(hash + '-menu').length > 0) {
-        $('#about-menu').removeClass('active');
-        $(hash + '-menu').addClass('active');
-    }
-
     $('#contacts-menu,#demo-menu').on('click',function(){
         $('#about-menu').removeClass('active');
         $('#contacts-menu').removeClass('active');
         $('#demo-menu').removeClass('active');
         $(this).addClass('active');
     })
-    $('a[href^="#"]').on('click', function(event) {
 
-        var target = $( $(this).attr('href') );
-
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').animate({
-                scrollTop: target.offset().top - 60
-            }, 500);
-        }
-
-    });
 	 $('.course-button').on('click', function(event) {
 
         var target = $( '#prices' );

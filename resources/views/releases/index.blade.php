fe8c29f892795c113cad0c6ef04d354c86ce2472
Eugene <viiper94@gmail.com>
2022-12-18 15:22:09 +0200

    fixing releases layout

--- resources/views/releases/index.blade.php
+++ resources/views/releases/index.blade.php
@@ -3,25 +3,26 @@
 @section('title', 'CTS Records - New Electronic Music')
 
 @section('content')
-    <div class = "container">
-        @include('admin.layout.alert')
-        <div class="col-md-9 col-sm-8 content">
-            <div class="row" style="margin-top: -4px;">
-            @foreach($releases as $release)
-                    <div class="col-sm-4 col-xs-6 release-brief">
-                        <a href="{{ route('release', $release->id) }}">
-                            <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="img-responsive"/>
-                            <div class="item-overlay">
-                                <div class="item-data">
-                                    <div>{{  $release->title  }}</div>
+    <div class="container pt-3">
+        <div class="row flex-nowrap">
+            <section class="col content">
+                <div class="row">
+                    @foreach($releases as $release)
+                        <div class="col-sm-4 col-xs-6 release-brief mb-4">
+                            <a href="{{ route('release', $release->id) }}">
+                                <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="img-fluid">
+                                <div class="item-overlay">
+                                    <div class="item-data">
+                                        <div>{{  $release->title  }}</div>
+                                    </div>
                                 </div>
-                            </div>
-                        </a>
-                    </div>
-                @endforeach
-            </div>
-            {{ $releases->links('layout.pagination') }}
+                            </a>
+                        </div>
+                    @endforeach
+                </div>
+                {{ $releases->links('layout.pagination') }}
+            </section>
+            @include('layout.aside')
         </div>
-        @include('layout.aside')
     </div>
 @endsection

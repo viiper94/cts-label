fd9bc8c4a17587a623d0859a02855eeff4e88b44
Eugene <viiper94@gmail.com>
2022-12-22 19:23:29 +0200

    fixed artists page layout

--- resources/views/artists.blade.php
+++ resources/views/artists.blade.php
@@ -3,25 +3,23 @@
 @section('title', 'CTS Studio Artists')
 
 @section('content')
-    <div class = "container">
-        <div class="col-md-9 col-sm-8 content">
-            <div class="row" style="padding:0 30px 0 10px; margin-bottom:30px;">
-                <div class="a-b">@lang('artists.artists_remixers')</div>
-                @foreach($artists as $artist)
-                <div class="col-sm-3 col-xs-6 artist-brief">
-                    <div class="row">
-                        <a href="{{ $artist->link ?? '#' }}" @unless($artist->link) target="_blank" @endif>
-                            <img src="/images/artists/{{ $artist->image }}" alt="{{ $artist->name }}"
-                                 class="img-responsive" width="100%" height="100%">
-                            <div class="artists-title">{{ $artist->name }}</div>
-                        </a>
-                    </div>
+    <div class="container pt-3 artists">
+        <div class="row">
+            <section class="col pe-5">
+                <div class="a-b mt-3 mb-4 text-uppercase">@lang('artists.artists_remixers')</div>
+                <div class="d-flex flex-wrap mb-3">
+                    @foreach($artists as $artist)
+                        <div class="artist-brief">
+                            @if($artist->link) <a href="{{ $artist->link }}" target="_blank">@endif
+                                <img src="/images/artists/{{ $artist->image }}" alt="{{ $artist->name }}" class="img-fluid">
+                                <div class="artist-title">{{ $artist->name }}</div>
+                            @if($artist->link)</a>@endif
+                        </div>
+                    @endforeach
                 </div>
-                @endforeach
-                <div class="clearfix"></div>
-            </div>
-            {{ $artists->links('layout.pagination') }}
+                {{ $artists->links('layout.pagination') }}
+            </section>
+            @include('layout.aside')
         </div>
-        @include('layout.aside')
     </div>
 @endsection

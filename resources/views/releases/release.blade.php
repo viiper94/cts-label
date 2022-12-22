acfbf62184780566c955f6299ec38b43f4b6fdea
Eugene <viiper94@gmail.com>
2022-12-22 11:37:54 +0200

    releases page fixed layout

--- resources/views/releases/release.blade.php
+++ resources/views/releases/release.blade.php
@@ -3,92 +3,84 @@
 @section('title', $release->title)
 
 @section('content')
-    <div class = "container">
-        <div class="col-md-9 col-sm-8 content" >
-            <div class="row" style="padding:0 17px;">
-                <div class="col-xs-12 col-md-7" >
-                    <h1 class="release-title">{{ $release->title }}</h1>
-                    <div class="release-number"><strong>@lang('releases.release_number') </strong>{{ $release->release_number }}</div>
-                    <div class="release-date"><strong>@lang('releases.release_date') </strong>{{ $release->release_date->format('j F Y') }}</div>
-                </div>
-                <div class="col-xs-12 col-md-5" >
-                    <div class="text-center">
-                        <a @if($prev) href="{{ route('release', $prev->id) }}" @endif class="prev-btn search-btns @if(!$prev)btn-disabled @endif">&nbsp;</a>
-                        <span class="release-search">@lang('releases.search_release')</span>
-                        <a @if($next)href="{{ route('release', $next->id) }}" @endif class="next-btn search-btns @if(!$next)btn-disabled @endif">&nbsp;</a>
+    <div class="container pt-3">
+        <div class="row">
+            <section class="col pe-5">
+                <div class="row">
+                    <div class="col-xs-12 col-md-7">
+                        <h1 class="release-title">{{ $release->title }}</h1>
+                        <div class="release-number"><strong>@lang('releases.release_number') </strong>{{ $release->release_number }}</div>
+                        <div class="release-date"><strong>@lang('releases.release_date') </strong>{{ $release->release_date->format('j F Y') }}</div>
                     </div>
-                    <div class="text-right switch-btns">
-                        @if($release->detectActiveDescriptionLang(true) > 1)
-                            @if($release->getUsefulText($release->description_en))
-                                <a class="switch-btn pull-right @if($release->detectActiveDescriptionLang() === 'en') active @endif"
-                                    data-lang="en" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">@lang('shared.en')</a>
+                    <div class="col-xs-12 col-md-5">
+                        <div class="text-center">
+                            <a @if($prev)href="{{ route('release', $prev->id) }}" @endif class="prev-btn search-btns @if(!$prev)btn-disabled @endif">&nbsp;</a>
+                            <span class="release-search">@lang('releases.search_release')</span>
+                            <a @if($next)href="{{ route('release', $next->id) }}" @endif class="next-btn search-btns @if(!$next)btn-disabled @endif">&nbsp;</a>
+                        </div>
+                        <div class="lang-switch pb-3">
+                            @if($release->detectActiveDescriptionLang(count: true) > 1)
+                                @if($release->getUsefulText($release->description_ua))
+                                    <a class="switch-btn @if($release->detectActiveDescriptionLang() === 'ua') active @endif"
+                                       data-lang="ua" href="{{ route('release', $release->id) }}">@lang('shared.ua')</a>
+                                @endif
+                                @if($release->getUsefulText($release->description_ru))
+                                    <a class="switch-btn @if($release->detectActiveDescriptionLang() === 'ru') active @endif"
+                                       data-lang="ru" href="{{ route('release', $release->id) }}">@lang('shared.ru')</a>
+                                @endif
+                                @if($release->getUsefulText($release->description_en))
+                                    <a class="switch-btn @if($release->detectActiveDescriptionLang() === 'en') active @endif"
+                                        data-lang="en" href="{{ route('release', $release->id) }}">@lang('shared.en')</a>
+                                @endif
                             @endif
-                            @if($release->getUsefulText($release->description_ru))
-                                <a class="switch-btn pull-right @if($release->detectActiveDescriptionLang() === 'ru') active @endif"
-                                   data-lang="ru" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">@lang('shared.ru')</a>
-                            @endif
-                            @if($release->getUsefulText($release->description_ua))
-                                <a class="switch-btn pull-right @if($release->detectActiveDescriptionLang() === 'ua') active @endif"
-                                   data-lang="ua" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">@lang('shared.ua')</a>
-                            @endif
-                        @endif
-                    </div>
-                </div>
-                <div class="clearfix"></div>
-                <div class="col-xs-12 col-md-7">
-                    <figure>
-                        <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="release-image img-responsive"/>
-                    </figure>
-                    <div class="release-buttons">
-                        <a @if($release->youtube) href="{{ $release->youtube }}" @endif target="_blank" class="share share-play @if(!$release->youtube) btn-disabled @endif"></a>
-                        <a @if($release->beatport) href="{{ $release->beatport }}" @endif target="_blank" class="share share-beatport @if(!$release->beatport) btn-disabled @endif"></a>
-                        <a href="javascript:void(0)" onclick="shareSocial('fb')" class="share share-facebook"></a>
-                        <a href="javascript:void(0)" onclick="shareSocial('twitter')" class="share share-twitter"></a>
-                        <div id="share-share" class="share share-share" style="position: relative;" data-toggle="collapse" data-target="#collapsed-social">
-                            <div id="collapsed-social" class="collapse">
-{{--                                <a href="javascript:void(0)" onclick="shareSocial('vk')" class="collapsed-share share-vk"></a>--}}
-                                <a href="javascript:void(0)" onclick="shareSocial('lj')" class="collapsed-share share-lj"></a>
-                                <a href="javascript:void(0)" onclick="shareSocial('pin')" class="collapsed-share share-pin"></a>
-                                <a href="javascript:void(0)" onclick="shareSocial('mail')" class="collapsed-share share-mail"></a>
-                            </div>
                         </div>
                     </div>
-                    <div class="release-tracklist">
-                        <div class="inner-tracklist">
+                </div>
+                <div class="row">
+                    <div class="col-xs-12 col-md-7">
+                        <figure>
+                            <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="release-image img-fluid w-100"/>
+                        </figure>
+                        <div class="release-buttons d-flex justify-content-between py-5">
+                            <a @if($release->youtube) href="{{ $release->youtube }}" @endif target="_blank" class="share share-youtube @if(!$release->youtube) btn-disabled @endif"></a>
+                            <a @if($release->beatport) href="{{ $release->beatport }}" @endif target="_blank" class="share share-beatport @if(!$release->beatport) btn-disabled @endif"></a>
+                            <a href="javascript:void(0)" onclick="shareSocial('fb')" class="share share-facebook"></a>
+                            <a href="javascript:void(0)" onclick="shareSocial('twitter')" class="share share-twitter"></a>
+                            <a href="javascript:void(0)" onclick="shareSocial('mail')" class="share share-mail"></a>
+                        </div>
+                        <div class="release-tracklist">
                             @if($release->tracklist)
-                            <b>@lang('releases.tracklist')</b><br/>
-                            {!! $release->tracklist !!}
+                                <h6 class="fw-bold">@lang('releases.tracklist')</h6>
+                                {!! $release->tracklist !!}
                             @endif
                         </div>
                     </div>
-                </div>
-                <div class="release-content col-xs-12 col-md-5">
-                    <div class="content-ru">
+                    <div class="col-xs-12 col-md-5 release-content">
                         {!! $release['description_'.$release->detectActiveDescriptionLang()] !!}
                     </div>
                 </div>
-                <div class="clearfix"></div>
-                @if(count($release->related) > 0)
-                    <div class="col-xs-12 release-related">
-                        <span>@lang('releases.related_releases')</span>
-                        @foreach($release->related as $item)
-                            <div class="col-md-4 col-sm-6 col-xs-6 release-brief release-brief-related">
-                                <a href="{{ route('release', $item->id) }}">
-                                    <img src="/images/releases/{{ $item->image }}" alt="{{ $item->title }}" class="img-responsive"/>
-                                    <div class="item-overlay">
-                                        <div class="item-data">
-                                            <div>{{ $item->title }}</div>
-                                        </div>
+                @if($release->related)
+                    <div class="row py-5">
+                        <div class="col-12 release-related">
+                            <h6 class="mb-3">@lang('releases.related_releases')</h6>
+                            <div class="d-flex justify-content-between flex-wrap">
+                                @foreach($release->related as $item)
+                                    <div class="release-brief release-brief-related">
+                                        <a href="{{ route('release', $item->id) }}" class="d-block">
+                                            <img src="/images/releases/{{ $item->image }}" alt="{{ $item->title }}" class="img-fluid">
+                                            <div class="item-overlay d-flex justify-content-center align-items-center p-3 text-center">
+                                                <span class="">{{ $item->title }}</span>
+                                            </div>
+                                        </a>
                                     </div>
-                                </a>
+                                @endforeach
                             </div>
-                        @endforeach
+                        </div>
                     </div>
                 @endif
-            </div>
-            <div class="clearfix"></div>
+            </section>
+            @include('layout.aside')
         </div>
-        @include('layout.aside')
     </div>
 @endsection
 

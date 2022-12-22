d612f8f47e05abd7e4b968deceb19dd430c1bab5
Eugene <viiper94@gmail.com>
2022-12-22 15:32:30 +0200

    fixed footer icons

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -18,20 +18,46 @@
             </div>
             <div class="col-xs-12 col-md-6 col-sm-5">
                 <p class="text-end">@lang('footer.follow_us')</p>
-                <div class="text-end">
-                    <a href="https://beatport.com/label/cts-creative-technologies-studio/4470" class="socials socials-beatport" target="_blank"></a>
-                    <button type="button" class="socials socials-itunes"></button>
-                    <a href="https://facebook.com/CTS.Records/" class="socials socials-facebook" target="_blank"></a>
-                    <a href="https://youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" class="socials socials-youtube" target="_blank"></a>
-                    <a href="https://ra.co/labels/2317" class="socials socials-ra" target="_blank"></a>
-                    <a href="https://soundcloud.com/cts-records" class="socials socials-soundcloud" target="_blank"></a>
-                    <a href="https://last.fm/music/CTS+Records" class="socials socials-as" target="_blank"></a>
-                    <button type="button" class="socials socials-rss"></button>
-                    <a href="https://twitter.com/CTS_RECORDS" class="socials socials-twitter" target="_blank"></a>
-                    <button type="button" class="socials socials-instagram"></button>
-                    <a href="https://discogs.com/label/74862-CTS-records" class="socials socials-discogs" target="_blank"></a>
-                    <a href="https://open.spotify.com/user/892tia21rho36ip7xqygpqxpq" class="socials socials-spotify" target="_blank"></a>
-                    <a href="https://myspace.com/cts_records" class="socials socials-myspace" target="_blank"></a>
+                <div class="d-flex justify-content-end">
+                    <a href="https://beatport.com/label/cts-creative-technologies-studio/4470" class="socials socials-beatport" target="_blank">
+                        <i class="icon-beatport"></i>
+                    </a>
+                    <button type="button" class="socials socials-itunes">
+                        <i class="fa-brands fa-itunes-note"></i>
+                    </button>
+                    <a href="https://facebook.com/CTS.Records/" class="socials socials-facebook" target="_blank">
+                        <i class="fa-brands fa-facebook-f"></i>
+                    </a>
+                    <a href="https://youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" class="socials socials-youtube" target="_blank">
+                        <i class="fa-brands fa-youtube"></i>
+                    </a>
+                    <a href="https://ra.co/labels/2317" class="socials socials-ra" target="_blank">
+                        <i class="icon-resident-advisor"></i>
+                    </a>
+                    <a href="https://soundcloud.com/cts-records" class="socials socials-soundcloud" target="_blank">
+                        <i class="fa-brands fa-soundcloud"></i>
+                    </a>
+                    <a href="https://last.fm/music/CTS+Records" class="socials socials-as" target="_blank">
+                        <i class="fa-brands fa-lastfm"></i>
+                    </a>
+                    <button type="button" class="socials socials-rss">
+                        <i class="fa-solid fa-rss"></i>
+                    </button>
+                    <a href="https://twitter.com/CTS_RECORDS" class="socials socials-twitter" target="_blank">
+                        <i class="fa-brands fa-twitter"></i>
+                    </a>
+                    <button type="button" class="socials socials-instagram">
+                        <i class="fa-brands fa-instagram"></i>
+                    </button>
+                    <a href="https://discogs.com/label/74862-CTS-records" class="socials socials-discogs" target="_blank">
+                        <i class="icon-discogs"></i>
+                    </a>
+                    <a href="https://open.spotify.com/user/892tia21rho36ip7xqygpqxpq" class="socials socials-spotify" target="_blank">
+                        <i class="fa-brands fa-spotify"></i>
+                    </a>
+                    <a href="https://myspace.com/cts_records" class="socials socials-myspace" target="_blank">
+                        <i class="icon-myspace"></i>
+                    </a>
                 </div>
             </div>
         </div>

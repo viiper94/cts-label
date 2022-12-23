c527179e28e51ba540478a16ade4d14b18a92da6
Eugene <viiper94@gmail.com>
2022-12-23 13:42:22 +0200

    fixed scrollspy when on about page

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -24,7 +24,7 @@
                         <a @class(['nav-link', 'active' => \Route::is('home') || \Route::is('release')]) href="{{ route('home') }}">@lang('navbar.home')</a>
                     </li>
                     <li class="nav-item">
-                        <a @class(['nav-link', 'active' => \Route::is('about')]) href="{{ route('about') }}" id="about-menu">@lang('navbar.about')</a>
+                        <a @class(['nav-link', 'active' => \Route::is('about')]) href="{{ \Route::is('about') ? '#about' : route('about') }}" id="about-menu">@lang('navbar.about')</a>
                     </li>
                     <li class="nav-item">
                         <a @class(['nav-link', 'active' => \Route::is('artists')]) href="{{ route('artists') }}">@lang('navbar.artists')</a>

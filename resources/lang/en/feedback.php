d95e86dbbd99aecf53bf044de3360aaf2c285221
Eugene <viiper94@gmail.com>
2022-12-23 08:44:49 +0200

    added translation files for feedback page

---
+++ resources/lang/en/feedback.php
@@ -0,0 +1,7 @@
+<?php
+return [
+
+    'header_text' => 'Please let us know what you think by filling out this feedback form! After filling out our feedback
+                    form and sending your reaction by pushing "Send Feedback" button, you will get download links on new page.',
+
+];

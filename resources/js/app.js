5ae8abcc22841157fe8534e8edb62c22084cf7c5
Eugene <viiper94@gmail.com>
2022-12-23 08:44:29 +0200

    added wavesurfer package

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,7 @@
 import "bootstrap/dist/js/bootstrap.min";
 import "scrolltofixed/jquery-scrolltofixed";
 import Litepicker from "litepicker"
+import WaveSurfer from "wavesurfer.js";
 
 require('./public/common.js');
 require('./public/navbar.js');

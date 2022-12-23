9b681ec5697ee4b8b1edd887585daa788cdf87e5
Eugene <viiper94@gmail.com>
2022-12-23 13:42:30 +0200

    fixed about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,68 +3,78 @@
 @section('title', 'About CTS')
 
 @section('content')
-    <div class = "container" data-bs-spy="scroll" data-bs-target="#main_nav" data-bs-smooth-scroll="true">
-        <div class="col-md-9 col-sm-8 content inner">
-            <div class="row" style="padding:0 30px 0 0;">
-                <script type="application/ld+json">
-                    {
-                      "@context": "https://schema.org",
-                      "@type": "Organization",
-                      "name": "CTS Label",
-                      "url" : "https://www.cts-label.com",
-                      "logo": "https://www.cts-label.com/assets/img/logo.png",
-                      "address": {
-                        "@type": "PostalAddress",
-                        "addressLocality": "Kyiv, Ukraine",
-                        "streetAddress": "Бессарабська площа, 4,"
-                      },
-                      "email": "info(at)cts-label.com",
 
-                     "telephone": "+38 098 685 40 33"
-                    }
-                </script>
-                <div>
-                    <h1>@lang('navbar.about')</h1>
+    <script type="application/ld+json">
+        {
+          "@context": "https://schema.org",
+          "@type": "Organization",
+          "name": "CTS Label",
+          "url" : "https://www.cts-label.com",
+          "logo": "https://www.cts-label.com/assets/images/logo.png",
+          "address": {
+            "@type": "PostalAddress",
+            "addressLocality": "Kyiv, Ukraine",
+            "streetAddress": "Бессарабська площа, 4,"
+          },
+          "email": "info(at)cts-label.com",
+
+         "telephone": "+38 098 685 40 33"
+        }
+    </script>
+
+    <div class="container about pt-3" data-bs-spy="scroll" data-bs-target="#main_nav" data-bs-smooth-scroll="true">
+        <div class="row">
+            <section class="col pe-5">
+                <div id="about">
+                    <h5 class="py-3">@lang('navbar.about')</h5>
                     @lang('about.about')
                 </div>
-                <div class="popup">
+                <div class="pb-5">
                     <p>
-                        <a data-target="#collapsable" data-toggle="collapse" aria-expanded="false" href="#subscribe">
+                        <a data-target="#collapsable" data-bs-toggle="collapse" aria-expanded="false" href="#subscribe" aria-controls="subscribe">
                             @lang('about.subscribe')
                         </a>
                     </p>
-                    <form action="#" id="collapsable" method="POST" name="subscribe-email" class="collapse">
-                        <input name="name" required type="text" class="popup-form form-control" placeholder="@lang('about.subscribe_name')">
-                        <input name="email" required type="email" class="popup-form form-control" placeholder="@lang('about.subscribe_email')">
-                        <input type="submit" value="@lang('about.subscribe_submit')" class="popup-form btn">
+                    <form id="subscribe" method="POST" name="subscribe-email" class="collapse">
+                        <div class="form-group row">
+                            <div class="col">
+                                <input name="name" required type="text" class="form-control form-control-sm form-dark" placeholder="@lang('about.subscribe_name')">
+                            </div>
+                            <div class="col">
+                                <input name="email" required type="email" class="form-control form-control-sm form-dark" placeholder="@lang('about.subscribe_email')">
+                            </div>
+                            <div class="col">
+                                <input type="submit" value="@lang('about.subscribe_submit')" class="btn btn-outline btn-sm">
+                            </div>
+                        </div>
                     </form>
                 </div>
-                <div id="demo">
-                    <h1>@lang('about.demo_policy')</h1>
+                <div id="demo" class="py-5">
+                    <h5 class="py-3">@lang('about.demo_policy')</h5>
                     @lang('about.demo')
                 </div>
-                <div id="contacts">
-                    <h1>@lang('navbar.contact')</h1>
-                    <div class="cts-records">
-                        <div class="grey">@lang('about.contact_cts_studio_requests')</div>
+                <div id="contacts" class="pt-5">
+                    <h5 class="pt-3">@lang('navbar.contact')</h5>
+                    <div class="pt-3 pb-5 cts-records">
+                        <h6>@lang('about.contact_cts_studio_requests')</h6>
                         <p>@lang('about.contact_general_info') <a href="mailto:info@cts-studio.com">info@cts-studio.com</a></p>
                         <p>@lang('about.contact_bookings') <a href="mailto:info@cts-studio.com">info@cts-studio.com</a></p>
                         <p>@lang('about.contact_a_and_r') <a href="mailto:info@sergiomega.com">info@sergiomega.com</a></p>
                         <p>@lang('about.contact_licencing') <a href="mailto:info@cts-studio.com">info@cts-studio.com</a></p>
                     </div>
-                    <div class="cts-school">
-                        <div class="grey">@lang('about.contact_cts_school_requests')</div>
+                    <div class="pt-3 pb-5 cts-school">
+                        <h6>@lang('about.contact_cts_school_requests')</h6>
                         <p>@lang('about.contact_email') <a href="mailto:info@cts-studio.com">info@cts-label.com</a></p>
-                        <p>@lang('about.contact_t') <a href="tel:+38 098 685 40 33">+38 098 685 40 33</a></p>
+                        <p>@lang('about.contact_t') <a href="tel:+380986854033">+380 98 685 40 33</a></p>
                     </div>
-                    <div class="cts-studio">
-                        <div class="grey">CTS Studio requests:</div>
+                    <div class="pt-3 pb-5 cts-studio">
+                        <h6>CTS Studio requests:</h6>
                         <p>@lang('about.contact_email') <a href="mailto:info@cts-studio.com">info@cts-studio.com</a></p>
-                        <p>@lang('about.contact_t') <a href="tel:+38 098 685 40 33">+38 098 685 40 33</a></p>
+                        <p>@lang('about.contact_t') <a href="tel:+380986854033">+380 98 685 40 33</a></p>
                     </div>
                 </div>
-            </div>
+            </section>
+            @include('layout.aside')
         </div>
-        @include('layout.aside')
     </div>
 @endsection

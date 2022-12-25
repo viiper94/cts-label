000d05b5c2e2e98843a6e16a215d09c9b2778078
Eugene <viiper94@gmail.com>
2022-12-25 23:31:11 +0200

    fixed studio page layout

--- resources/views/studio.blade.php
+++ resources/views/studio.blade.php
@@ -1,11 +1,5 @@
 @extends('layout.layout')
 
-@section('assets')
-    <link href="/assets/css/ctssc2.css" rel="stylesheet" />
-    <link  href="/assets/css/styles.css" rel="stylesheet">
-    <script type="text/javascript" src="/assets/js/modernizr.js"></script>
-@endsection
-
 @section('title')
     @lang('studio.page_header')
 @endsection
@@ -36,6 +30,7 @@
 @endsection
 
 @section('content')
+
     <script type="application/ld+json">
     {
       "@context": "https://schema.org",
@@ -51,119 +46,152 @@
       "telephone": "+38 098 685 40 33"
     }
     </script>
-    <div class="col-xs-12 studio-page">
-        <div class="row">
-            <div class="col-xs-12 s-logo">
-                <div class="container">
-                    <img src="/assets/img/cts-studio-y.png" alt="CTStudio">
-                    <div class="pull-right">
-                        <div class="text-right switch-btns" style="padding-right:18px;">
-                            <a class="switch-btn pull-right @if(isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'en') active @endif"
-                               data-lang="en" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">
-                                @lang('shared.en')
-                            </a>
-                            <a class="switch-btn pull-right @if(isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'ru') active @endif"
-                               data-lang="ru" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">
-                                @lang('shared.ru')
-                            </a>
-                            <a class="switch-btn pull-right @if((isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'ua') || !isset($_COOKIE['lang'])) active @endif"
-                               data-lang="ua" href="{{!$_SERVER['QUERY_STRING'] ? '' : '?'.$_SERVER['QUERY_STRING']}}">
-                                @lang('shared.ua')
-                            </a>
-                        </div>
+
+    <div class="studio">
+        <div class="studio-header py-5">
+            <div class="container py-3">
+                <div class="lang-switch">
+                    <div class="btn-group">
+                        <a @class(['btn switch-btn', 'active' => isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'en'])
+                           data-lang="en" href="{{ route('studio') }}">
+                            @lang('shared.en')
+                        </a>
+                        <a @class(['btn switch-btn', 'active' => isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'ua' || !isset($_COOKIE['lang'])])
+                           data-lang="ua" href="{{ route('studio') }}">
+                            @lang('shared.ua')
+                        </a>
+                        <a @class(['btn switch-btn', 'active' => isset($_COOKIE['lang']) && $_COOKIE['lang'] === 'ru'])
+                           data-lang="ru" href="{{ route('studio') }}">
+                            @lang('shared.ru')
+                        </a>
                     </div>
                 </div>
             </div>
         </div>
-        <div>
-            <div class="row">
-                <div class="school-menu studio-land">
-                    <div class="container">
-                        <ul class="list-unstyled s-menu studio-menu">
-                            <li><a href="#equipment">@lang('studio.equipment')</a></li>
-                            <li><a href="#services">@lang('studio.services')</a></li>
-                            <li><a href="#projects">@lang('studio.projects')</a></li>
-                            <li><a href="#contacts">@lang('studio.contacts')</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            <div class="clearfix"></div>
+        <div class="studio-nav">
             <div class="container">
-                <div class="text col-xs-12 col-md-12" style="margin-top: 60px;">
-                    <div class="row">@lang('studio.top_text')</div>
-                </div>
-                <div class="studio-images ">
-                    <section class="b-marquee-line">
-                        <div class="b-marquee-line__flow">
-                            <div class="b-marquee-line__flow-in">
-                                <span class="b-marquee-line__flow-block">
-                                    @for($i = 1; $i <= 8; $i++)
-                                       <img src="/assets/img/studio-{{ $i }}.jpg" class="pull-left" />
-                                    @endfor
-                                </span>
-                                <span class="b-marquee-line__flow-block">
-                                    @for($i = 1; $i <= 8; $i++)
-                                        <img src="/assets/img/studio-{{ $i }}.jpg" class="pull-left" />
-                                    @endfor
-                                </span>
+                <ul class="py-3 px-0">
+                    <li><a href="#equipment">@lang('studio.equipment')</a></li>
+                    <li><a href="#services">@lang('studio.services')</a></li>
+                    <li><a href="#projects">@lang('studio.projects')</a></li>
+                    <li><a href="#contacts">@lang('studio.contacts')</a></li>
+                </ul>
+            </div>
+        </div>
+        <div class="studio-content">
+            <div class="container">
+                <section class="description">
+                    <div class="text pt-5">
+                        @lang('studio.top_text')
+                    </div>
+                    <div class="marquee overflow-hidden mb-3">
+                        <div class="marquee-flow d-flex">
+                        <span class="d-flex">
+                            @for($i = 1; $i <= 8; $i++)
+                                <img src="/images/studio-{{ $i }}.jpg">
+                            @endfor
+                        </span>
+                            <span class="d-flex">
+                            @for($i = 1; $i <= 8; $i++)
+                                    <img src="/images/studio-{{ $i }}.jpg">
+                                @endfor
+                        </span>
+                        </div>
+                    </div>
+                    <div class="text">
+                        @lang('studio.middle_text')
+                    </div>
+                </section>
+                <section class="equipment py-5">
+                    <h1 class="text-center text-uppercase fw-bold mb-5" id="equipment">@lang('studio.equipment')</h1>
+                    <div class="row">
+                        <div class="col-12 col-sm-6">
+                            <p class="equipment-header text-uppercase py-2 m-auto">
+                                @lang('studio.equip_main')
+                            </p>
+                            <div class="equipment-text pt-5">
+                                @lang('studio.main_studio')
                             </div>
                         </div>
-                    </section>
-                    <div class="clearfix"></div>
-                </div>
-                <div class="text col-xs-12 col-md-12">
-                    <div class="row">@lang('studio.middle_text')</div>
-                </div>
-                <div class="clearfix"></div>
-                <h2 class="text-center sh uppercase" id="equipment">@lang('studio.equipment')</h2>
-                <div class="col-xs-12">
-                    <div class="col-md-3 col-md-offset-2 col-sm-4 grey-text hrdw">
-                        <div class="row">
-                            <div class="bordered uppercase">@lang('studio.equip_main')</div>
-                            @lang('studio.main_studio')
+                        <div class="col-12 col-sm-6">
+                            <p class="equipment-header text-uppercase py-2 m-auto">
+                                @lang('studio.equip_dj')
+                            </p>
+                            <div class="equipment-text pt-5">
+                                @lang('studio.dj_studio')
+                            </div>
                         </div>
                     </div>
-
-                    <div class="col-md-3 col-md-offset-3 col-sm-4 col-sm-offset-4 grey-text hrdw">
-                        <div class="row">
-                            <div class="bordered uppercase">@lang('studio.equip_dj')</div>
-                            @lang('studio.dj_studio')
-                        </div>
-                    </div>
-                    <div class="clearfix"></div>
-                    <h2 class="text-center sh uppercase" id="services">@lang('studio.services')</h2>
-                    <div class="service-images ">
+                </section>
+                <section class="services py-5">
+                    <h1 class="text-center text-uppercase fw-bold mb-5" id="services">@lang('studio.services')</h1>
+                    <div class="service-images d-flex justify-content-between px-5 flex-wrap">
                         @foreach($services as $service)
-                            <div class="col-md-3 col-sm-4 text-center scale{{ $loop->iteration }}">
-                                <a href="mailto:info@cts-label.com?body=@lang('mail.my_name')%0A @lang('mail.my_phone')%0A&Subject=@lang('studio.services') - {{ $service->name }}">
+                            <div class="service-item m-3">
+                                <a data-bs-toggle="modal" data-bs-target="#service-modal" data-name="{{ $service->name }}" class="service-link">
                                     <img src="/images/studio/services/{{ $service->image }}"
                                          class="service-image" @if($service->alt) alt="{{ $service->alt }}" @endif>
                                 </a>
                             </div>
                         @endforeach
-                        <div class="clearfix"></div>
                     </div>
-                    <ul class="list-inline slist slist-en">
+                    <ul class="px-5 py-3 mb-0">
                         <li>@lang('studio.service_1');</li>
                         <li>@lang('studio.service_2');</li>
                         <li>@lang('studio.service_3').</li>
                     </ul>
-                    <a class="text-center download" href="http://www.studio.cts.ua/CTS_studio_price_list.PDF"
-                       target="_blank">@lang('studio.dl_price')</a>
-
-                    <h2 class="sh text-center uppercase" id="projects">@lang('studio.projects')</h2>
-                    <div class="text">@lang('studio.bottom_text')</div>
-                    <h2 class="text-center sh uppercase" id="contacts">@lang('studio.contacts')</h2>
-                    <div class="text-center col-xs-12">
-                        <div style="font-size: 15px; max-width: 200px; display: inline-block;">
-                            @lang('studio.contacts_text')
-                            <a href="mailto:info@cts-label.com" class="wl"
-                                          style="text-transform: lowercase">info@cts-label.com</a></p>
-                        </div>
+                    <div class="d-flex justify-content-center">
+                        <a class="text-center text-uppercase price-list" href="/CTS_studio_price_list.PDF" target="_blank">@lang('studio.dl_price')</a>
                     </div>
-                </div>
+                </section>
+                <section class="projects py-5">
+                    <h1 class="text-center text-uppercase fw-bold mb-5" id="projects">@lang('studio.projects')</h1>
+                    <div class="text">@lang('studio.bottom_text')</div>
+                </section>
+                <section class="contacts pt-5">
+                    <h1 class="text-center text-uppercase fw-bold mb-5" id="contacts">@lang('studio.contacts')</h1>
+                    <div class="text-center">
+                        @lang('studio.contacts_text')
+                        <a href="mailto:info@cts-label.com">info@cts-label.com</a>
+                    </div>
+                </section>
             </div>
         </div>
     </div>
+
+    <div class="modal fade" id="service-modal" tabindex="-1" aria-labelledby="modalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+                <form method="post">
+                    <div class="modal-header">
+                        <h1 class="modal-title fs-5" id="modalLabel">@lang('studio.modal.header')</h1>
+                    </div>
+                    <div class="modal-body">
+                        @csrf
+                        <div class="form-group mb-3">
+                            <label for="service" class="form-label">@lang('studio.modal.service')</label>
+                            <input type="text" name="service" id="service" class="form-control form-dark" readonly>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="name" class="form-label">@lang('studio.modal.your_name')*</label>
+                            <input type="text" name="name" id="name" class="form-control form-dark" required>
+                        </div>
+                        <div class="form-group mb-3">
+                            <label for="tel" class="form-label">@lang('studio.modal.your_tel')</label>
+                            <input type="tel" name="tel" id="tel" class="form-control form-dark">
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="form-label">@lang('studio.modal.your_email')*</label>
+                            <input type="email" name="email" id="email" class="form-control form-dark" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">@lang('studio.modal.submit')</button>
+                        <button type="button" class="btn btn-outline" data-bs-dismiss="modal"><i class="fa-solid fa-xmark"></i></button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+
 @endsection

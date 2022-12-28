4ff7dc303260c661664b25aeff77f51479190f72
Eugene <viiper94@gmail.com>
2022-12-28 08:41:16 +0200

    fixed releases grid classes

--- resources/views/releases/index.blade.php
+++ resources/views/releases/index.blade.php
@@ -6,16 +6,19 @@
     <div class="container pt-3">
         <div class="row">
             <section class="col">
-                <div class="d-flex flex-wrap">
+                <div class="row g-1">
                     @foreach($releases as $release)
-                        <div class="release-brief mb-4">
-                            <a href="{{ route('release', $release->id) }}" class="d-block">
-                                <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="img-fluid">
-                                <div class="item-overlay d-flex justify-content-center align-items-center p-3 text-center">
-                                    <span class="">{{ $release->title }}</span>
-                                </div>
-                            </a>
+                        <div class="col-md-4 mb-4 g-2">
+                            <div class="release-brief">
+                                <a href="{{ route('release', $release->id) }}" class="d-block">
+                                    <img src="/images/releases/{{ $release->image }}" alt="{{ $release->title }}" class="img-fluid">
+                                    <div class="item-overlay d-flex justify-content-center align-items-center p-3 text-center">
+                                        <span class="">{{ $release->title }}</span>
+                                    </div>
+                                </a>
+                            </div>
                         </div>
+
                     @endforeach
                 </div>
                 {{ $releases->links('layout.pagination') }}

9a3e67a7d5c3828cd2cb16311cd4042654b1ded3
Eugene <viiper94@gmail.com>
2022-12-28 10:12:01 +0200

    responsive fixes

--- resources/views/layout/spotify.blade.php
+++ resources/views/layout/spotify.blade.php
@@ -1,6 +1,6 @@
 <div class="widget">
     <h3>TOP TEN</h3>
     <div class="widget-content">
-        <iframe src="https://open.spotify.com/embed/playlist/1fNpo3tDYQpTSqe9V94JMy?theme=0" width="300" height="420" frameBorder="0" allowfullscreen="" allow="encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+        <iframe src="https://open.spotify.com/embed/playlist/1fNpo3tDYQpTSqe9V94JMy?theme=0" width="100%" height="420" frameBorder="0" allowfullscreen="" allow="encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
 </div>

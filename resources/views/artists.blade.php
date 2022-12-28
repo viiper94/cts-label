ec8853a77c339b3bde7444b0dd09fc3a26c3702c
Eugene <viiper94@gmail.com>
2022-12-28 13:56:03 +0200

    responsive fixes

--- resources/views/artists.blade.php
+++ resources/views/artists.blade.php
@@ -5,11 +5,11 @@
 @section('content')
     <div class="container pt-3 artists">
         <div class="row">
-            <section class="col pe-5">
+            <section class="col pe-md-5">
                 <h5 class="a-b mt-3 mb-4">@lang('artists.artists_remixers')</h5>
-                <div class="d-flex flex-wrap mb-3">
+                <div class="row g-0 mb-3">
                     @foreach($artists as $artist)
-                        <div class="artist-brief">
+                        <div class="artist-brief col-6 col-sm-4 col-md-4 col-lg-3 g-0">
                             @if($artist->link) <a href="{{ $artist->link }}" target="_blank">@endif
                                 <img src="/images/artists/{{ $artist->image }}" alt="{{ $artist->name }}" class="img-fluid">
                                 <div class="artist-title">{{ $artist->name }}</div>

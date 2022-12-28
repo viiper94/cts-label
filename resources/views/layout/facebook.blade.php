b19bf8daa6af6e9f7dc6ef99365c5acbe5fddab7
Eugene <viiper94@gmail.com>
2022-12-28 13:37:12 +0200

    fixed width

--- resources/views/layout/facebook.blade.php
+++ resources/views/layout/facebook.blade.php
@@ -1,6 +1,6 @@
 <div class="widget">
     <h3><a href="https://www.facebook.com/CTS.Records/?fref=ts">FACEBOOK</a></h3>
     <div class="widget-content">
-        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCTS.Records%2F&tabs=timeline&width=310&height={{ env('FACEBOOK_HEIGHT') ?? 'auto' }}&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=1586615888304509" width="100%" height="{{ env('FACEBOOK_HEIGHT') ?? 'auto' }}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+        <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FCTS.Records%2F&tabs=timeline&width=300&height={{ env('FACEBOOK_HEIGHT') ?? 'auto' }}&small_header=true&adapt_container_width=true&hide_cover=false&show_facepile=false&appId=1586615888304509" width="100%" height="{{ env('FACEBOOK_HEIGHT') ?? 'auto' }}" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
     </div>
 </div>

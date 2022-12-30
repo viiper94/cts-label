9f5cba3816b1fbc9a9a1634125db2fad7b9c221f
Eugene <viiper94@gmail.com>
2022-12-30 16:19:15 +0200

    fixed unsubscribe page

--- resources/views/emails/unsubscribe.blade.php
+++ resources/views/emails/unsubscribe.blade.php
@@ -2,31 +2,27 @@
 
 @section('content')
 
-    <div class="container unsubscribe">
-        <form method="post">
+    <div class="container unsubscribe py-5">
+        <form method="post" class="py-5">
             @csrf
-            <div class="panel panel-default">
-                <div class="panel-heading"><h4>@lang('emailing.unsubscribe.unsubscribe_from')</h4></div>
-                <div class="panel-body panel-body__dark">
-                    <h4>@lang('emailing.unsubscribe.email') <b>{{ $email }}</b></h4>
-                    <h4>@lang('emailing.unsubscribe.receive_message') <b>{{ $from }}</b></h4>
+            <div class="card text-bg-dark w-50 m-auto">
+                <div class="card-header"><h5 class="card-title mb-0">@lang('emailing.unsubscribe.unsubscribe_from')</h5></div>
+                <div class="card-body">
+                    <p class="mb-0">@lang('emailing.unsubscribe.email') <b>{{ $email }}</b></p>
+                    <p>@lang('emailing.unsubscribe.receive_message') <b>{{ $from }}</b></p>
 
-                    <p style="margin-top: 40px"><b>@lang('emailing.unsubscribe.you_can')</b></p>
-                    <div class="radio">
-                        <label for="current">
-                            <input type="radio" name="type" value="current" id="current" checked>
-                            @lang('emailing.unsubscribe.unsubscribe_current')
-                        </label>
+                    <p class="mb-0"><b>@lang('emailing.unsubscribe.you_can')</b></p>
+                    <div class="form-check">
+                        <input type="radio" name="type" value="current" id="current" class="form-check-input" checked>
+                        <label for="current" class="form-check-label">@lang('emailing.unsubscribe.unsubscribe_current')</label>
                     </div>
-                    <div class="radio">
-                        <label for="all">
-                            <input type="radio" name="type" value="all" id="all">
-                            @lang('emailing.unsubscribe.unsubscribe_all')
-                        </label>
+                    <div class="form-check">
+                        <input type="radio" name="type" value="all" id="all" class="form-check-input">
+                        <label for="all" class="form-check-label">@lang('emailing.unsubscribe.unsubscribe_all')</label>
                     </div>
                 </div>
-                <div class="panel-footer">
-                    <button type='submit' class='btn'>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-outline">
                         <span class='glyphicon glyphicon-ok' aria-hidden='true'></span>
                         @lang('emailing.unsubscribe.unsubscribe')
                     </button>

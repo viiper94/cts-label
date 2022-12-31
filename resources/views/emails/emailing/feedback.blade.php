92afdb6aca2f20e46bac3864981611c630c638c3
Eugene <viiper94@gmail.com>
2022-12-31 20:30:57 +0200

    fixed feedback email

--- resources/views/emails/emailing/feedback.blade.php
+++ resources/views/emails/emailing/feedback.blade.php
@@ -4,18 +4,38 @@
         <tbody>
         <tr>
             <td>
-                <img src="https://www.cts-label.com/images/email/logo_rassilka2.jpg" alt="CTS Sites" style="width: 700px; height: 122px;">
+                <table style="margin: auto">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <a href="{{ route('school') }}" target="_blank">
+                                <img src="{{ url('/') }}/images/email/ctschool.png" alt="CTSchool" height="122">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="{{ route('home') }}" target="_blank">
+                                <img src="{{ url('/') }}/images/email/cts.png" alt="CTS" height="122">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://sergiomega.com/" target="_blank">
+                                <img src="{{ url('/') }}/images/email/sergio-mega.png" alt="Sergio Mega" height="122">
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
             </td>
         </tr>
         <tr>
             <td style="text-align: center;">
-                <h3>Dear {{ $name }}!</h3>
+                <h2>Dear {{ $name }}!</h2>
                 <h1>We need your feedback!</h1>
             </td>
         </tr>
         <tr>
             <td style="text-align: center;">
-                <h6 style="color: blue">DOWNLOAD TRACKS FOR FREE AND SEND US YOUR REACTIONS FOR USING ON SEVERAL WEBSITES</h6>
+                <p style="color: blue;font-size: 13px;"><b>DOWNLOAD TRACKS FOR FREE AND SEND US YOUR REACTIONS FOR USING ON SEVERAL WEBSITES</b></p>
             </td>
         </tr>
         <tr>
@@ -24,21 +44,25 @@
                 @if($mail->feedback->release?->genre)
                     <h3>Genre: {{ $mail->feedback->release->genre }}</h3>
                 @endif
-                {!! $mail->feedback->release->description_en !!}
+                <div style="font-size: 16px;">
+                    {!! $mail->feedback->release->description_en !!}
+                </div>
             </td>
         </tr>
         <tr>
             <td>
-                @if(!$mail->feedback->release)
-                    <img src="{{ url('/') }}/images/feedback/{{ $mail->feedback->image ?? 'default.png' }}" alt="{{ $mail->feedback->feedback_title }}">
-                @else
-                    <img src="{{ url('/') }}/images/releases/{{ $mail->feedback->release->image ?? 'default.png' }}" alt="{{ $mail->feedback->feedback_title }}">
-                @endif
+                <a href="{{ route('feedback', $mail->feedback->slug) }}" target="_blank">
+                    @if(!$mail->feedback->release)
+                        <img src="{{ url('/') }}/images/feedback/{{ $mail->feedback->image ?? 'default.png' }}" alt="{{ $mail->feedback->feedback_title }}" width="250">
+                    @else
+                        <img src="{{ url('/') }}/images/releases/{{ $mail->feedback->release->image ?? 'default.png' }}" alt="{{ $mail->feedback->feedback_title }}" width="250">
+                    @endif
+                </a>
             </td>
         </tr>
         <tr>
             <td>
-                <h3><a href="{{ route('feedback', $mail->feedback->slug) }}" target="_blank" style="color: blue;">Download & feedback</a></h3>
+                <h3><a href="{{ route('feedback', $mail->feedback->slug) }}" target="_blank" style="color: blue;font-size: 19px;">Download & feedback</a></h3>
             </td>
         </tr>
         @if(count($mail->feedback->related) > 0)
@@ -47,7 +71,7 @@
                     <p>Also available:</p>
                     <p>
                         @foreach($mail->feedback->related as $item)
-                            <a href="{{ route('feedback', $item->slug) }}" target="_blank" style="color: red;">{{ $item->feedback_title }}</a>
+                            <a href="{{ route('feedback', $item->slug) }}" target="_blank" style="color: red;"><b>{{ $item->feedback_title }}</b></a>
                             <br>
                         @endforeach
                     </p>
@@ -62,59 +86,68 @@
             </td>
         </tr>
         <tr>
-            <p style="font-size: 11px">Follow us</p>
-            <table style="text-align: center">
-                <tbody>
-                <tr>
-                    <td>
-                        <a href="https://beatport.com/label/cts-creative-technologies-studio/4470" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-beatport.png" alt="Follow us on Beatport">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://facebook.com/CTS.Records/" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-facebook.png" alt="Follow us on Facebook">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-youtube.png" alt="Follow us on YouTube">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://ra.co/labels/2317" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-ra.png" alt="Follow us on Resident Advisor">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://soundcloud.com/cts-records" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-soundcloud.png" alt="Follow us on Soundcloud">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://twitter.com/CTS_RECORDS" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-twitter.png" alt="Follow us on Twitter">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://discogs.com/label/74862-CTS-records" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-discogs.png" alt="Follow us on Discogs">
-                        </a>
-                    </td>
-                    <td>
-                        <a href="https://open.spotify.com/user/892tia21rho36ip7xqygpqxpq" target="_blank">
-                            <img src="{{ url('/') }}/images/email/social-spotify.png" alt="Follow us on Spotify">
-                        </a>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+            <td>
+                <span style="font-size: 11px">Follow us</span>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <table style="text-align: center; margin: auto">
+                    <tbody>
+                    <tr>
+                        <td>
+                            <a href="https://beatport.com/label/cts-creative-technologies-studio/4470" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-beatport.png" alt="Follow us on Beatport" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://facebook.com/CTS.Records/" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-facebook.png" alt="Follow us on Facebook" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-youtube.png" alt="Follow us on YouTube" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://ra.co/labels/2317" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-ra.png" alt="Follow us on Resident Advisor" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://soundcloud.com/cts-records" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-soundcloud.png" alt="Follow us on Soundcloud" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://twitter.com/CTS_RECORDS" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-twitter.png" alt="Follow us on Twitter" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://discogs.com/label/74862-CTS-records" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-discogs.png" alt="Follow us on Discogs" width="40">
+                            </a>
+                        </td>
+                        <td>
+                            <a href="https://open.spotify.com/user/892tia21rho36ip7xqygpqxpq" target="_blank">
+                                <img src="{{ url('/') }}/images/email/socials-spotify.png" alt="Follow us on Spotify" width="40">
+                            </a>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </td>
         </tr>
         @if($hash)
             <tr>
-                <p style="font-size: 12px; font-style: italic">You get this e-mail because you are a part of international music industry.
-                    We respect you and your inbox! If you feel disturbed by our newsletter,
-                    push <a href="{{ route('unsubscribe', $hash) }}" target="_blank">unsubscribe</a> and you will immediately be...</p>
+                <td>
+                    <p style="font-size: 12px; font-style: italic">You get this e-mail because you are a part of international music industry.
+                        <br>
+                        We respect you and your inbox! If you feel disturbed by our newsletter,<br>
+                        push <a href="{{ route('unsubscribe', $hash) }}" target="_blank">unsubscribe</a> and you will immediately be...</p>
+                </td>
             </tr>
         @endif
         </tbody>

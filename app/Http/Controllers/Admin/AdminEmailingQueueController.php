06a244e05c6071d8e4b2564365b32b38b6adaaee
Eugene <viiper94@gmail.com>
2023-01-04 19:40:25 +0200

    added search in emailing queue

--- app/Http/Controllers/Admin/AdminEmailingQueueController.php
+++ app/Http/Controllers/Admin/AdminEmailingQueueController.php
@@ -6,13 +6,19 @@ use App\EmailingChannel;
 use App\EmailingContact;
 use App\EmailingQueue;
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
 
 class AdminEmailingQueueController extends Controller{
 
-    public function index(){
+    public function index(Request $request){
+        $queue = EmailingQueue::with('channel');
+        if($request->input('q')){
+            $queue = $queue->where('name', 'like', '%'.$request->input('q').'%')
+                ->orWhere('to', 'like', '%'.$request->input('q').'%');
+        }
         return view('admin.emailing.queue', [
             'channels' => EmailingChannel::all(),
-            'queue' => EmailingQueue::with('channel')->orderBy('sent')->orderBy('sort')->paginate(100),
+            'queue' => $queue->orderBy('sent')->orderBy('sort')->paginate(100),
         ]);
     }
 

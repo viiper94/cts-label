639f1f8ff71b36e88296d2b81e0c15bacf686e94
Eugene <viiper94@gmail.com>
2023-01-11 20:12:12 +0200

    fixed search form route always to index

--- resources/views/admin/layout/search.blade.php
+++ resources/views/admin/layout/search.blade.php
@@ -1,4 +1,6 @@
-<form role="search" enctype="application/x-www-form-urlencoded">
+<form role="search" enctype="application/x-www-form-urlencoded" class="input-group"
+    action="{{ route(preg_replace('/([a-zA-Z_]+)$/', 'index', \Request::route()->getName())) }}">
     <input type="text" name="q" class="form-control form-dark" id="search" placeholder="Поиск" value="{{ Request::input('q') }}">
+    <button type="submit" class="btn btn-outline"><i class="fa-solid fa-magnifying-glass"></i></button>
 </form>
 <hr>

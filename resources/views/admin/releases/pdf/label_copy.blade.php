f6e47970283f89d0024bc1614a250ab2ace75582
Eugene <viiper94@gmail.com>
2023-01-14 15:12:40 +0200

    added label copy document template view

--- resources/views/admin/releases/pdf/label_copy.blade.php
+++ resources/views/admin/releases/pdf/label_copy.blade.php
@@ -1,6 +1,37 @@
-<div class="wrapper">
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
 
-    <img src="{{ public_path('images/label_copy_header.jpg') }}">
+    <style>
+        .wrapper{
+            font-family: 'DejaVu Sans', 'sans-serif';
+            font-style: normal;
+            margin-left: 100px;
+            margin-top: 40px;
+        }
+        p{
+            font-size: 16px;
+        }
+        p.fs-18{
+            font-size: 18px;
+        }
+        .info{
+            margin-top: 60px;
+            padding-left: 60px;
+        }
+        img{
+            width: 100%;
+        }
+        .mt-0{
+            margin-top: 0;
+        }
+    </style>
+
+</head>
+<body class="wrapper">
+
+    <img src="{{ resource_path('img/label_copy_header.jpg') }}">
 
     <span class="mb-0">Label copy:</span><br>
     <p class="fs-18 mt-0">{{ $track->artists }} - {{ $track->name }} @if($track->mix_name)({{ $track->mix_name }})@endif</p>
@@ -8,7 +39,7 @@
     <div class="info">
         <p>Artist: <b>{{ $track->artists }}</b></p>
         <p>Title: <b>{{ $track->name }}</b></p>
-        @if(count($track->remixers) > 0)
+        @if($track->remixers && $track->remixers[0] !== '')
             <p>Remixer: <b>{{ implode(', ', $track->remixers) }}</b></p>
         @endif
         <p>Remix: <b>{{ $track->mix_name ?? 'Original Mix' }}</b></p>
@@ -20,36 +51,13 @@
         <p>Published by <b>Atal Music</b></p>
         <p>Territory: <b>World excluding Korea</b></p>
         <p>Booking: <a href="mailto:info@cts-studio.com">info@cts-studio.com</a></p>
-        <p>&copy; 2010 - {{ date('Y') }} CTS records</p>
+        <p>&#8471;&&copy; 2010 - {{ date('Y') }} CTS records</p>
         <p>
-            <a href="https://cts-studio.com">www.cts-studio.com</a><br>
+            <a href="{{ route('home') }}">www.cts-studio.com</a><br>
             <a href="{{ route('home') }}">www.cts-label.com</a>
         </p>
     </div>
 
-</div>
+</body>
 
-<style>
-    .wrapper{
-        font-family: Arial, sans-serif;
-        font-style: normal;
-        margin-left: 100px;
-        margin-top: 40px;
-    }
-    p{
-        font-size: 16px;
-    }
-    p.fs-18{
-        font-size: 18px;
-    }
-    .info{
-        margin-top: 60px;
-        padding-left: 60px;
-    }
-    img{
-        width: 100%;
-    }
-    .mt-0{
-        margin-top: 0;
-    }
-</style>
+</html>

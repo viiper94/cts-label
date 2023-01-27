ce9f97d34a888061d173a3c87ad9bf8bac66a075
Eugene <viiper94@gmail.com>
2023-01-27 21:13:25 +0200

    fixed alert positioning

--- resources/views/admin/layout/alert.blade.php
+++ resources/views/admin/layout/alert.blade.php
@@ -1,5 +1,5 @@
 @if(session()->get('success'))
-    <div class="toast alert-toast align-items-center position-absolute border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
+    <div class="toast alert-toast align-items-center position-fixed border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
         <div class="toast-header text-bg-success">
             <strong class="me-auto">Успех!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
@@ -11,7 +11,7 @@
 @endif
 
 @if($errors->any())
-    <div class="toast alert-toast text-bg-dark align-items-center border-0 m-3 top-0 end-0 position-absolute" role="alert" aria-live="assertive" aria-atomic="true">
+    <div class="toast alert-toast text-bg-dark align-items-center border-0 m-3 top-0 end-0 position-fixed" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
         <div class="toast-header text-bg-danger">
             <strong class="me-auto">Ошибка =(</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>

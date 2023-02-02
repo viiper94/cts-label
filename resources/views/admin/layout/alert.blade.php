9963f2cd8ed4773f665f05ce937047510e4f88d6
Eugene <viiper94@gmail.com>
2023-02-02 21:26:10 +0200

    reworded toasts headers

--- resources/views/admin/layout/alert.blade.php
+++ resources/views/admin/layout/alert.blade.php
@@ -1,7 +1,7 @@
 @if(session()->get('success'))
     <div class="toast alert-toast align-items-center position-fixed border-0 m-3 top-0 end-0" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
         <div class="toast-header text-bg-success">
-            <strong class="me-auto">Успех!</strong>
+            <strong class="me-auto">Успіх!</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">
@@ -13,7 +13,7 @@
 @if($errors->any())
     <div class="toast alert-toast text-bg-dark align-items-center border-0 m-3 top-0 end-0 position-fixed" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="3000">
         <div class="toast-header text-bg-danger">
-            <strong class="me-auto">Ошибка =(</strong>
+            <strong class="me-auto">От халепа =(</strong>
             <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
         </div>
         <div class="toast-body">

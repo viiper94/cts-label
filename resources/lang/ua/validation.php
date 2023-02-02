e10dd4f5579197dc68d7bfcc6641a2582bf363e0
Eugene <viiper94@gmail.com>
2023-02-02 21:25:37 +0200

    translated validation of attributes on event page

---
+++ resources/lang/ua/validation.php
@@ -0,0 +1,61 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'email' => 'Поле :attribute повинно містити правильну email адресу.',
+    'max' => [
+        'string' => 'Поле :attribute не повинно мати більше :max символів.',
+    ],
+    'required' => 'Поле :attribute є обов\'язкове.',
+    'required_if' => 'Поле :attribute є обов\'язкове якщо обрано :value.',
+    'string' => 'Поле :attribute повинно бути тестовим рядком.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap our attribute placeholder
+    | with something more reader friendly such as "E-Mail Address" instead
+    | of "email". This simply helps us make our message more expressive.
+    |
+    */
+
+    'attributes' => [
+        'email' => 'E-mail',
+        'name' => 'Ім\'я, Прізвище',
+        'type' => 'Статус',
+        'tel' => 'Контактний телефон',
+        'additional' => 'Додаткова інформація',
+        'other' => 'інше',
+    ],
+
+];

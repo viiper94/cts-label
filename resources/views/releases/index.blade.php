b1e45b621e96354bed066011c9a2f6e846c9d3ef
Eugene <viiper94@gmail.com>
2023-02-06 20:17:25 +0200

    added seo meta for main page

--- resources/views/releases/index.blade.php
+++ resources/views/releases/index.blade.php
@@ -30,3 +30,66 @@
         </div>
     </div>
 @endsection
+
+@section('meta')
+    <!-- OG Meta tags -->
+    <meta property="og:title" content="CTS Records">
+    <meta property="og:type" content="music.record_label">
+    <meta property="og:url" content="https://cts-label.com/">
+    <meta property="og:image" content="https://cts-label.com/images/logo.png">
+    <meta property="og:description" content="CTS - Creative Technologies Studio - one of the first independent record labels based in Ukraine working in electronic dance music sphere.">
+
+    <!-- Twitter Meta tags -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@CTS_RECORDS">
+    <meta name="twitter:title" content="CTS Records">
+    <meta name="twitter:description" content="CTS - Creative Technologies Studio - one of the first independent record labels based in Ukraine working in electronic dance music sphere.">
+    <meta name="twitter:image" content="https://cts-label.com/images/logo.png">
+
+@endsection
+
+@section('json-ld')
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "MusicGroup",
+  "name": "CTS Records",
+  "url": "https://www.famousrecords.com/",
+  "sameAs": [
+    "https://www.facebook.com/famousrecords",
+    "https://www.twitter.com/famousrecords",
+    "https://www.instagram.com/famousrecords"
+  ],
+  "logo": "https://www.famousrecords.com/images/logo.png",
+  "image": "https://www.famousrecords.com/images/header.jpg",
+  "musicGenre": "Pop, Rock, R&B, Hip-Hop",
+  "description": "Famous Records is a leading record label, known for discovering and promoting the best talent in popular music genres.",
+  "album": [
+    {
+      "@type": "MusicAlbum",
+      "name": "Summer Hits",
+      "byArtist": {
+        "@type": "MusicGroup",
+        "name": "Various Artists"
+      },
+      "image": "https://www.famousrecords.com/images/albums/summerhits.jpg",
+      "genre": "Pop",
+      "releaseDate": "2022-06-01",
+      "recordLabel": "Famous Records"
+    },
+    {
+      "@type": "MusicAlbum",
+      "name": "Rockin' the Night",
+      "byArtist": {
+        "@type": "MusicGroup",
+        "name": "The Rockers"
+      },
+      "image": "https://www.famousrecords.com/images/albums/rockinthenight.jpg",
+      "genre": "Rock",
+      "releaseDate": "2022-07-15",
+      "recordLabel": "Famous Records"
+    }
+  ]
+}
+</script>
+@endsection

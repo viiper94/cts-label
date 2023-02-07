7b7c308e0f23621028615952f63ac6e99ddca9b6
Eugene <viiper94@gmail.com>
2023-02-07 22:01:22 +0200

    added widgets lazy loading

--- resources/views/layout/youtube.blade.php
+++ resources/views/layout/youtube.blade.php
@@ -1,6 +1,6 @@
 <div class="widget">
     <h3><a href="https://www.youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" target="_blank" rel="noreferrer">YOUTUBE</a></h3>
     <div class="widget-content">
-        <iframe width="100%" height="210" src="https://www.youtube.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen></iframe>
+        <iframe width="100%" height="210" src="https://www.youtube.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen loading="lazy"></iframe>
     </div>
 </div>

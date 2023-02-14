f2c21fa37d2147533634648ca0ae6ed61d3c8219
Eugene <viiper94@gmail.com>
2023-02-14 21:36:53 +0200

    fixed users page layout

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,44 +1,32 @@
 @extends('admin.layout.layout')
 
+@section('title')
+    Пользователи | CTS Records Admin Panel
+@endsection
+
 @section('admin-content')
 
-    <div class="container-fluid">
+    <div class="container-fluid users">
         @include('admin.layout.alert')
-        @csrf
-        <div class="table-responsive">
-            <table class="items-table table table-hover table__dark">
-                <tbody>
-                    <tr>
-                        <th></th>
-                        <th>Имя</th>
-                        <th>E-Mail</th>
-                        <th>Статус</th>
-                        <th>Зарегестрирован</th>
-                        <th>Действия</th>
-                    </tr>
+        <div class="table-responsive my-3" data-fl-scrolls>
+            <table class="items-table table table-hover table-dark">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th>Имя</th>
+                    <th>E-Mail</th>
+                    <th>Статус</th>
+                    <th>Зарегестрирован</th>
+                </tr>
+                </thead>
+                <tbody class="text-nowrap">
                     @foreach($users as $user)
                         <tr>
-                            <td><img src="/images/users/{{ $user->image ?? 'default.png' }}"></td>
+                            <td><img src="/images/users/{{ $user->image ?? 'default.png' }}" class="img-fluid" style="height: 30px"></td>
                             <td>{{ $user->name }}</td>
                             <td>{{ $user->email }}</td>
-                            <td><span class="label {{ $user->getStatus()['labelClass'] }}">{{ $user->getStatus()['name'] }}</span></td>
+                            <td><span class="badge {{ $user->status->badgeClass() }}">{{ $user->status->name() }}</span></td>
                             <td>{{ $user->created_at->format('j M Y, H:i') }}</td>
-                            <td>
-                                @if(!$user->is_admin)
-                                    <form action="{{ route('users.destroy', $user->id) }}" method="post">
-                                        @method('DELETE')
-                                        @csrf
-                                    </form>
-                                    <a class='btn btn-success' href='{{ route('users.index') }}/set/{{ $user->id }}/student'>
-                                        <span class='glyphicon glyphicon-pencil' aria-hidden='true'></span>
-                                        <span class="hidden-xs hidden-sm hidden-lg">Назначить студентом</span>
-                                    </a>
-                                    <button class='btn btn-danger' onclick='return confirm("Удалить пользователя и все его данные?")'>
-                                        <span class='glyphicon glyphicon-trash' aria-hidden='true'></span>
-                                        <span class="hidden-xs hidden-sm hidden-lg">Удалить</span>
-                                    </button>
-                                @endif
-                            </td>
                         </tr>
                     @endforeach
                 </tbody>

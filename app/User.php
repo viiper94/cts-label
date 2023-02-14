7aecd2a515b200336b7f7027215ca3164b26be79
Eugene <viiper94@gmail.com>
2023-02-14 21:34:54 +0200

    added user status enum

--- app/User.php
+++ app/User.php
@@ -2,7 +2,7 @@
 
 namespace App;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use App\Enums\UserStatus;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
@@ -10,54 +10,25 @@ class User extends Authenticatable
 {
     use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
     protected $fillable = [
         'name', 'email', 'password',
     ];
 
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
     protected $hidden = [
         'password', 'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-    ];
-    public $statusCodes = [
-        0 => [
-            'name' => 'Пользователь',
-            'labelClass' => 'label-default'
-        ],
-        1 => [
-            'name' => 'Студент',
-            'labelClass' => 'label-info'
-        ],
-        2 => [
-            'name' => 'Выпускник',
-            'labelClass' => 'label-success'
-        ],
+        'status' => UserStatus::class,
     ];
 
     public function cv(){
         return $this->hasOne('App\Cv');
     }
 
-    public function getStatus(){
-        if($this->is_admin) return ['name' => 'Aдминистратор', 'labelClass' => 'label-danger'];
-        return key_exists($this->status, $this->statusCodes) ? $this->statusCodes[$this->status] : $this->statusCodes[0];
+    public function isAdmin(){
+        return $this->is_admin && $this->status === 0;
     }
 
 }

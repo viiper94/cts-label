8df76a074f2dd3f7954ebca4b99f70cb1393cb08
Eugene <viiper94@gmail.com>
2023-02-15 22:35:23 +0200

    added event email view, added image

--- resources/views/emails/emailing/custom.blade.php
+++ resources/views/emails/emailing/custom.blade.php
@@ -1,31 +1,38 @@
-<p>Dear {{ $name }}, dear friends, colleagues and partners!</p>
+<x-mail::message>
 
-<p>Here is Sergiy Lazarev aka Sergio Mega - CTS Records Head of Label/A&R. <br>
-    Our CEO Lilia Lazaryeva and me are open for meetings at ADE.</p>
+## Вебінар: «Маркетинг та менеджмент артиста в кризові часи» <br>участь безкоштовна
 
-<p>This year we are focused on: <br>
-    - licensing out our catalogue (over 1400 house, deep house, tech-house, techno, progressive, chill-out tracks); <br>
-    - presenting our ADE 2022 sampler “Sound of Brave” of new tracks by Ukrainian artists made during wartime; <br>
-    - presenting our artists and remixers roster; <br>
-    - this year as always, we are looking for new outstanding underground house  and techno productions for our label; <br>
-    - we will also be able to share with you the latest and true news about the situation in our home country Ukraine</p>
 
-<p>We will stay in Amsterdam from Wednesday 19th till Friday 21 of October, please let us know 3 time slots suitable for you. <br>
-    As always we prefer to arrange our meetings in the Felix Meritis.</p>
+![Маркетинг та менеджмент артиста в кризові часи](https://cts-label.com/images/marketing-and-management.jpg)
 
-<p>Looking forward to hearing from you.</p>
+Ви займаєтеся музичним мистецтвом? Ви автор музики? Виконавець? Музикант? Звукорежисер? Менеджер артиста? <br>
+Тоді цей вебінар для Вас! <br>
+Проект «Маркетинг і менеджмент артиста в кризові часи» призваний допомогти українським митцям втілити
+свої творчі проекти та знайти додаткові джерела фінансування під час війни.
 
-<p>Kind regards,</p>
+Спікери вебінару: **Лілія Лазарєва, Денис Васильєв, Sergio Mega** – експерти, професіонали музичної індустрії,
+поділяться з Вами власним досвідом та вже реалізованими кейсами і дадуть відповіді на питання.
 
-<p>Sergiy Lazarev aka Sergio Mega</p>
+**Основні тези вебінару:**<br>
+*- Грантові програми для українських митців, діячів музичної сфери.* <br>
+*- Участь у фестивалях та шоукейсах.* <br>
+*- Створення міжнародних культурних проєктів. Особливості їх менеджменту.* <br>
+*- Міжнародні музичні резиденції. Що це таке і навіщо в них брати участь?* <br>
+*- Робота автора з ОКУ.*<br>
+*- Сучасні технології просування музичних творів.*<br>
+*- Видання музичних творів – практичні поради.*
 
-<p>Head of Label, A&R <br>
-    CTS Records <br>
-    Kyiv, Ukraine</p>
+<span class="text-red">Дата проведення</span> **21 лютого 2023 року**, <span class="text-red">початок о </span> **18.00**
+<br><span class="text-red">Тривалість</span> **2 години** <br>
+*Вебінар проходитиме на платформі Zoom*
 
-<p>WhatsApp +38 067 703 15 72 <br>
-    <a href="mailto:info@cts-studio.com">info@cts-studio.com</a> <br>
-    <a href="https://www.cts-label.com" target="_blank">https://www.cts-label.com</a> <br>
-    <a href="https://www.sergiomega.com" target="_blank">https://www.sergiomega.com</a> <br>
-    <a href="https://www.facebook.com/CTS.Records" target="_blank">https://www.facebook.com/CTS.Records</a> <br>
-    <a href="https://www.facebook.com/sergio.mega.cts" target="_blank">https://www.facebook.com/sergio.mega.cts</a></p>
+*Реєстрація до участі обов'язкова. <br>
+Процес реєстрації дуже простий і не забере більше двох хвилин.
+
+<x-mail::button :url="'https://cts-label.com/event/marketing-and-management'">
+    ЗАРЕЄСТРУВАТИСЬ НА ВЕБІНАР
+</x-mail::button>
+
+Будемо раді бачити Вас серед учасників вебінару!
+
+</x-mail::message>

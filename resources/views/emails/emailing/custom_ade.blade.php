63e924003d20a94da0c36ef2b03b20816f32a54e
Eugene <viiper94@gmail.com>
2023-02-15 22:35:37 +0200

    renamed old custom email view

---
+++ resources/views/emails/emailing/custom_ade.blade.php
@@ -0,0 +1,31 @@
+<p>Dear {{ $name }}, dear friends, colleagues and partners!</p>
+
+<p>Here is Sergiy Lazarev aka Sergio Mega - CTS Records Head of Label/A&R. <br>
+    Our CEO Lilia Lazaryeva and me are open for meetings at ADE.</p>
+
+<p>This year we are focused on: <br>
+    - licensing out our catalogue (over 1400 house, deep house, tech-house, techno, progressive, chill-out tracks); <br>
+    - presenting our ADE 2022 sampler “Sound of Brave” of new tracks by Ukrainian artists made during wartime; <br>
+    - presenting our artists and remixers roster; <br>
+    - this year as always, we are looking for new outstanding underground house  and techno productions for our label; <br>
+    - we will also be able to share with you the latest and true news about the situation in our home country Ukraine</p>
+
+<p>We will stay in Amsterdam from Wednesday 19th till Friday 21 of October, please let us know 3 time slots suitable for you. <br>
+    As always we prefer to arrange our meetings in the Felix Meritis.</p>
+
+<p>Looking forward to hearing from you.</p>
+
+<p>Kind regards,</p>
+
+<p>Sergiy Lazarev aka Sergio Mega</p>
+
+<p>Head of Label, A&R <br>
+    CTS Records <br>
+    Kyiv, Ukraine</p>
+
+<p>WhatsApp +38 067 703 15 72 <br>
+    <a href="mailto:info@cts-studio.com">info@cts-studio.com</a> <br>
+    <a href="https://www.cts-label.com" target="_blank">https://www.cts-label.com</a> <br>
+    <a href="https://www.sergiomega.com" target="_blank">https://www.sergiomega.com</a> <br>
+    <a href="https://www.facebook.com/CTS.Records" target="_blank">https://www.facebook.com/CTS.Records</a> <br>
+    <a href="https://www.facebook.com/sergio.mega.cts" target="_blank">https://www.facebook.com/sergio.mega.cts</a></p>

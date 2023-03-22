bf140daddfc6469dff2cd1007eb61b7fe4124b22
Eugene <viiper94@gmail.com>
2023-03-22 19:00:19 +0200

    fixed modal trigger function

--- resources/js/admin/studio_school.js
+++ resources/js/admin/studio_school.js
@@ -27,32 +27,18 @@ $(document).ready(function(){
         }
     });
 
-    $('.add-service').click(function(){
-        $('#serviceModal').find('#preview').attr('src', '/images/studio/services/default.png');
-        $('#serviceModal').find('[name=name]').val('');
-        $('#serviceModal').find('#service_alt').val('');
-        $('#serviceModal').find('#teacher_hinfo').val('');
-        $('#serviceModal').find('#teacher_binfo').val('');
-        $('#serviceModal').find('#visible').prop('checked', false);
-        $('#serviceModal').find('#lang option[value=en]').prop('selected', true);
-        $('#serviceModal').find('#modal-form').attr('action', $(this).data('action'));
-        $('#serviceModal #delete-form').hide();
-        if($('#serviceModal #modal-form [name=_method]').length > 0) $('#serviceModal #modal-form [name=_method]').remove();
-        $('#serviceModal').modal('show');
-    });
-
-    $('.service-lang .service-img, .service-lang .teacher button').click(function(){
-        $('#serviceModal').find('#preview').attr('src', $(this).attr('src') ?? $(this).data('src'));
-        $('#serviceModal').find('[name=name]').val($(this).data('name'));
-        $('#serviceModal').find('#service_alt').val($(this).attr('alt'));
-        $('#serviceModal').find('#teacher_hinfo').val($(this).data('hinfo'));
-        $('#serviceModal').find('#teacher_binfo').val($(this).data('binfo'));
-        $('#serviceModal').find('#visible').prop('checked', $(this).data('visible') == '1');
-        $('#serviceModal').find('#lang option[value='+$(this).data('lang')+']').prop('selected', true);
-        $('#serviceModal').find('#modal-form').attr('action', $(this).data('action'));
-        $('#serviceModal #delete-form').css('display', 'inline-block').attr('action', $(this).data('action'));
-        if($('#serviceModal #modal-form [name=_method]').length === 0) $('#serviceModal #modal-form').append('<input type="hidden" name="_method" value="PUT">');
-        $('#serviceModal').modal('show');
+    $('.service-lang .service-img, .service-lang .teacher button, .add-service').click(function(){
+        $btn = $(this);
+        $.ajax({
+            url: $btn.data('url'),
+            type: 'get',
+            success: function(response){
+                $('#editServiceModal .modal-body').remove();
+                $('#editServiceModal .modal-footer').remove();
+                $('#editServiceModal .modal-header').after(response.html);
+                $('#editServiceModal').modal('show');
+            }
+        });
     });
 
 });

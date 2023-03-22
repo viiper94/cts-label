cd58cb77aeb6120b9cc43250eaac289151637c8e
Eugene <viiper94@gmail.com>
2023-03-22 10:23:20 +0200

    added reviews btn and review modals

--- resources/views/admin/tracks/index.blade.php
+++ resources/views/admin/tracks/index.blade.php
@@ -80,6 +80,13 @@
                             @if($track->beatport_sample)
                                 <a href="{{ $track->beatport_sample }}" target="_blank" class="btn btn-sm btn-outline"><i class="fa-solid fa-play"></i></a>
                             @endif
+                            <button class="btn btn-sm btn-outline show-reviews" title="Ревью" data-url="{{ route('reviews.show', $track->id) }}">
+                                <i @class([
+                                        'bi',
+                                        'bi-star-fill text-primary' => $track->reviews_count > 0 || $track->also_supported_count > 0,
+                                        'bi-star' => $track->reviews_count == 0 && $track->also_supported_count == 0
+                                        ])></i>
+                            </button>
                             @if(count($track->releases) > 0)
                                 <a href="{{ route('releases.edit', $track->releases[0]->id) }}#tracklist" class="btn btn-sm btn-outline-primary">
                                     <i class="fa-solid fa-pen"></i>
@@ -102,6 +109,33 @@
         <div class="justify-content-center d-flex my-3">
             {{ $tracks->appends(Request::input())->links('admin.layout.pagination') }}
         </div>
+
+        <div class="modal fade" tabindex="-1" id="trackReviewsModal">
+            <div class="modal-dialog modal-xl modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Ревью</h5>
+                        <button type="button" class="btn btn-outline" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-times"></i></button>
+                    </div>
+                    <div class="modal-body">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="modal fade" tabindex="-1" id="editReviewModal">
+            <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <button type="button" class="btn btn-outline" data-bs-dismiss="modal" aria-label="Close"><i class="fa-solid fa-arrow-left"></i></button>
+                        <h5 class="modal-title flex-grow-1 ms-2">Редактирование ревью</h5>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+
     </div>
 
 @endsection

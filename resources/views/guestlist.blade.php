5aeb321f83392a4e727792f6bc5f1197c97a5450
Eugene <viiper94@gmail.com>
2023-10-09 11:43:09 +0300

    updated guestlist view

---
+++ resources/views/guestlist.blade.php
@@ -0,0 +1,18 @@
+@extends('layout.layout')
+
+@section('title', 'ADE 2023 Event Guest List')
+
+@section('content')
+
+    <div class="event-header py-3">
+        <div class="container py-5">
+            <h1>ADE 2023 Event Guest List</h1>
+        </div>
+    </div>
+
+
+    <div class="container event py-5">
+
+    </div>
+
+@endsection

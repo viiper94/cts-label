ddeb28a50a86119a5e3d564e56596a1611d3f309
Eugene <viiper94@gmail.com>
2023-10-10 23:28:18 +0300

    added prohor ade letter

---
+++ resources/views/emails/emailing/custom_ade_prohor.blade.php
@@ -0,0 +1,31 @@
+<p>Dear {{ $name }}!</p>
+
+<p>Here is Kateryna – manager of DJ & Producer Prohor (CTS Records, CWR Records). <br>
+    19th of October at ADE Prohor will
+    <a href="https://www.amsterdam-dance-event.nl/en/program/2023/xcm-records-25-years/2164965">
+        perform at NDSM Theater
+    </a>. <br>
+    I'm now compiling a day schedule for him. Prohor would like to meet with you at ADE. </p>
+
+<p>This year we are focused on: <br>
+    - Prohor’s bookings. We are looking for booking agent/agency for arranging and handling gigs requests for Europe and Asia.
+    Prohor is based in Prague and available for these territories. <br>
+    - Presenting Prohor’s ADE 2023 sampler. <br>
+    At the moment we have still open several slots on Thursday 19th in the morning and afternoon till 16.00 is also available. <br>
+    Please let me know if you would like to meet him in Amsterdam and suggest time slots suitable for you. <br>
+    He prefers to arrange his meetings at the Felix or close around. <br>
+    You can also catch Prohor after 20.00 in NDSM Theater after his performance.
+    If you like to get to the party just let me hear your names for the guest list.
+</p>
+
+<p>Looking forward to hearing from you.</p>
+
+<p>Kind regards,</p>
+
+<p>Kateryna <br>
+    Prohor Management <br>
+    ADE mob. +380669899604</p>
+
+<p><a href="mailto:prohor.music.ua@gmail.com">prohor.music.ua@gmail.com</a> <br>
+    <a href="https://www.facebook.com/prohoro4ek" target="_blank">https://www.facebook.com/prohoro4ek</a> <br>
+    <a href="https://open.spotify.com/artist/3MEHhrd8cFDd85o5Sg3Hr2" target="_blank">https://open.spotify.com/artist/3MEHhrd8cFDd85o5Sg3Hr2</a></p>

72773f7d696e112bb15e9fdc05bf47fc17c93565
Eugene <viiper94@gmail.com>
2023-10-10 11:16:40 +0300

    changed email and company fields nullable

--- database/migrations/2023_10_05_144649_create_guestlist_table.php
+++ database/migrations/2023_10_05_144649_create_guestlist_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('guestlist', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email');
-            $table->string('company');
+            $table->string('email')->nullable();
+            $table->string('company')->nullable();
             $table->timestamps();
         });
     }

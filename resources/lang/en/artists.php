90e8fe927e9c67a807b5c01633e67434ab8309f2
Eugene <viiper94@gmail.com>
2023-12-10 10:11:01 +0200

    added artists cv translations

--- resources/lang/en/artists.php
+++ resources/lang/en/artists.php
@@ -17,4 +17,44 @@ return [
     'link' => 'Link to Social Network',
 
     'artists_remixers' => 'ARTISTS / REMIXERS RELEASED A-Z',
+
+    'cv' => [
+        'title' => 'Artist Contact Info Form',
+        'artists_cv_page' => 'Artist Contact Info Form Page',
+        'not_public' => 'This info will NOT be made public!',
+        'tracks_to_sign' => 'Tracks to Sign',
+        'main_contact' => 'Main Contact',
+        'artists_info' => 'Artists Info',
+        'no_artist_yet' => 'There are no artists yet...',
+        'add_artists_info' => 'Add artist info',
+        'new_info' => 'Add artist info',
+        'edit_info' => 'Edit artist info',
+        'cv_created' => 'Thank you for filling up the form!',
+        'artists' => 'Artists',
+
+        'contact_name' => 'Contact Name',
+        'phone' => 'Phone number',
+        'artist_name' => 'Artist Name',
+        'first_name' => 'First Name',
+        'surname' => 'Surname',
+        'country' => 'Country',
+        'publisher' => 'Publisher',
+        'pro' => 'PRO',
+        'date_of_birth' => 'Date of Birth',
+        'address' => 'Address',
+        'city' => 'City/Town',
+        'state' => 'State',
+        'zip' => 'Postal/Zip Code',
+        'bank' => 'Bank name',
+        'place_of_bank' => 'Place of the Bank',
+        'account_holder' => 'Account holder',
+        'account_number' => 'Account number',
+        'passport_number' => 'Passport number',
+
+        'delete_info' => 'Remove this artists info',
+        'submit' => 'Submit this form',
+
+        'info_required' => 'At least 1 artist info is required',
+    ]
+
 ];

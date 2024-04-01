bc5b1fe6df1fdcc2055c46584215bfae9b56c2e6
Eugene <viiper94@gmail.com>
2024-04-01 09:22:49 +0300

    updated test

--- tests/Feature/AdminFeedbackResultTest.php
+++ tests/Feature/AdminFeedbackResultTest.php
@@ -2,19 +2,54 @@
 
 namespace Tests\Feature;
 
+use App\Feedback;
+use App\FeedbackResult;
+use App\FeedbackTrack;
+use App\Track;
+use App\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class AdminFeedbackResultTest extends TestCase
-{
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+class AdminFeedbackResultTest extends TestCase{
+    use RefreshDatabase;
+
+    public User $user;
+
+    protected function setUp(): void{
+        parent::setUp();
+
+        $this->user = User::factory()->create([
+            'is_admin' => true
+        ]);
+    }
+
+    public function test_process_review_button_action(): void{
+        $tracks = Track::factory(10)->create();
+        $feedback = Feedback::factory()->create();
+        foreach($tracks as $track){
+            FeedbackTrack::factory()->create([
+                'track_id' => $track->id,
+                'feedback_id' => $feedback->id
+            ]);
+        }
+        $result = FeedbackResult::factory()->create([
+            'feedback_id' => $feedback->id,
+            'best_track' => $tracks[fake()->randomDigit()]->getFullTitle()
+        ]);
+
+        $response = $this->actingAs($this->user)
+            ->ajaxGet(route('feedback.result.add', $result->id));
 
         $response->assertStatus(200);
+        $response->assertJsonCount(1);
+        $response->assertJsonSeeInOrder([
+            'name="track_id"',
+            'name="author"',
+            'name="review"',
+            'name="score"',
+            'name="result_accept"'
+        ]);
     }
+
 }

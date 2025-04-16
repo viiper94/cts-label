659c1e64973e2ab3f071db7c73ecbd59c513630d
Eugene <viiper94@gmail.com>
2025-04-16 10:14:29 +0300

    added new email template

---
+++ resources/views/emails/emailing/custom_ims_prohor.blade.php
@@ -0,0 +1,68 @@
+<style>
+    p{
+        color: #2c2c2c;
+        padding: 0 15px;
+    }
+</style>
+
+<div style="padding: 15px 0">
+
+    <p>Dear {{ $name }}!</p>
+
+    <p>Here is Olga – manager of DJ & Producer Prohor (CTS Records, Killer Mushrooms, CWR Records). 
+        Atlantis - the latest release of Prohor on Killer Mushrooms Records reached Beatport top 10 Melodic House & Techno. 
+        I'm now compiling a day IMS schedule for him. If you will be there Prohor would love to meet with you at IMS. 
+    </p>
+
+    <p>This year we are focused on: <br>
+        - Prohor’s bookings. We are looking for booking agent/agency for arranging and handling gig requests for Europe and Asia.
+        Prohor is based in Prague and available for these territories. You can check his upcoming tour dates below. <br>
+        - Presenting Prohor’s IMS 2025 sampler. <br>
+        At the moment he has still open several slots on Wednesday 23rd afternoon and Thursday 24th after 1.30pm till 16.30 is also available. 
+        Please let me know if you would like to meet Prohor in Ibiza and suggest time slots suitable for you. 
+        He prefers to arrange his meetings at the Mondrian at the lobby or close around. 
+    </p>
+
+    <p>Looking forward to hearing from you.</p>
+
+    <p>Kind regards,</p>
+
+    <p>Olga <br>
+        Prohor Management <br>
+        ADE mob./WhatsApp +380936299990</p>
+
+    <p><a href="mailto:prohor.music.ua@gmail.com">prohor.music.ua@gmail.com</a> <br>
+        <a href="https://www.instagram.com/prohor.music" target="_blank">https://www.instagram.com/prohor.music</a> <br>
+        <a href="https://open.spotify.com/artist/3MEHhrd8cFDd85o5Sg3Hr2" target="_blank">https://open.spotify.com/artist/3MEHhrd8cFDd85o5Sg3Hr2</a>
+    </p>
+    
+    <hr>
+
+    <p>shows: <br>
+        <a href="https://drive.google.com/file/d/1is1iALvo28VnCyVtklfaSiSA0aC3RMa8/view" target="_blank">Prohor @ ADE 2023</a>  <br>
+        <a href="https://drive.google.com/file/d/1ITToTbafu6GNNaPkU1Ps65h11vgaB0SN/view" target="_blank">Prohor @ Antalya Turkey 2024</a>
+    </p>
+
+    <hr>
+
+    <p>Upcoming Shows Spring-Summer 2025 </p>
+    <p>
+        26.04. Nalu Beach Bar Athens, Greece  <br>
+        08.05. Angel Atelier Warsaw, Poland  <br>
+        17.05. Isola Del Garda Lake Garda, Italy  <br>
+        20.05. Grande Real Villa Como, Italy  <br>
+        24.05. Brandza Open Air Bucharest, Romania  <br>
+        28.05. Palace Mala Wies Warsaw, Poland  <br>
+        07.06. Villa Aminta Lago Maggiore, Italy  <br>
+        11.06. Agricola Montechiaro Florence, Italy  <br>
+        16.06. Camper Bar Prague, Czech Republic  <br>
+        20.06. Castelfalfi, Pre-party Toscana, Italy  <br>
+        21.06. Castelfalfi Toscana, Italy  <br>
+        22.06. Castelfalfy Afterparty Toscana, Italy  <br>
+        14.06. Back Doors Bar, Prague, Czech Republic  <br>
+        08.08. Terrazzo Bar Split, Croatia  <br>
+        13.08. Bar a Boo Warsaw, Poland  <br>
+        20.08. Schloss Kartzow, Berlin Germany
+    </p>
+    
+</div>

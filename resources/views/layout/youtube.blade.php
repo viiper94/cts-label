21255bc15df485d05a4a8d658c51dda48f653a27
Eugene <viiper94@gmail.com>
2025-09-28 11:32:34 +0300

    added nowidgets option to load page without widgets

--- resources/views/layout/youtube.blade.php
+++ resources/views/layout/youtube.blade.php
@@ -1,6 +1,8 @@
 <div class="widget">
     <h3><a href="https://www.youtube.com/channel/UCudx-EMGd8zRddRAFWl7YHA" target="_blank" rel="noreferrer">YOUTUBE</a></h3>
     <div class="widget-content">
-        <iframe width="100%" height="210" src="https://www.youtube-nocookie.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen loading="lazy"></iframe>
+        @if(!Request::input('nowidgets'))
+            <iframe width="100%" height="210" src="https://www.youtube-nocookie.com/embed/3YtR-XcRFUI" frameborder="0" allowfullscreen loading="lazy"></iframe>
+        @endif
     </div>
 </div>

d5d8640e8834cc6bfa33a6c901fd7de5b0288227
eugenezaichuk <viiper94@gmail.com>
2025-10-18 11:52:09 +0300

    updated ade email

--- resources/views/emails/emailing/custom_ade.blade.php
+++ resources/views/emails/emailing/custom_ade.blade.php
@@ -1,28 +1,18 @@
 <p>Dear {{ $name }}, dear friends, colleagues and partners!</p>
 
 <p>Here is Sergiy Lazarev aka Sergio Mega - CTS Records Head of Label/A&R. <br>
-    Our CEO Lilia Lazaryeva and me are open for meetings at ADE.</p>
+    Our CEO Lilia Lazaryeva and I are open for meetings at ADE.</p>
 
 <p>This year we are focused on: <br>
-    - licensing out our catalogue (over 1400 house, deep house, tech-house, techno, progressive, chill-out tracks); <br>
-    - presenting our ADE 2023 sampler and our artists & remixers roster; <br>
+    - licensing out our catalogue (over 1500 house, deep house, tech-house, techno, progressive, chill-out tracks); <br>
+    - presenting our ADE 2025 sampler and our artists & remixers roster; <br>
     - this year as always, we are looking for new outstanding underground house and techno productions for our label; <br>
     - we will also be able to share with you the latest and true news about the situation in our home country Ukraine</p>
 
-<p>We will stay in Amsterdam from <b>Thursday 19th</b> till <b>Friday 20th</b> of October, please let us know 3 time slots suitable for you. <br>
-    Normally we prefer to arrange our meetings at the Felix Meritis entrance or close around.</p>
+<p>We will stay in Amsterdam from <b>Wednesday 22nd</b> till <b>Friday 24th</b> of October, please let us know 3 time slots suitable for you. <br>
+    Normally we prefer to arrange our meetings at the Andaz or close around.</p>
 
 <p>Looking forward to hearing from you.</p>
-
-<p>You can also catch Lilia and me at <a href="https://www.amsterdam-dance-event.nl/en/program/2023/xcm-records-25-years/2164965"><b>our event</b></a>
-    arranged in cooperation with XCM Records, <br> which will be held in
-    <a href="https://www.google.com/maps/place/NDSM+Theater+%2F+NDSM+Cafe/@52.4007778,4.8975371,17.13z/data=!4m6!3m5!1s0x47c60839b1afa55b:0xe04b32eddd29c405!8m2!3d52.4005949!4d4.8969948!16s%2Fg%2F11fzb0cb1f?authuser=0&entry=ttu">
-        <b>NDSM Theater</b>
-    </a> on <b>Thursday 19th</b>
-    with <b>Hanna Hais, Tito Torres, Bas Kunnen pres. NRG</b> and more. <br>
-    We start at <b>20.00</b> so this could be a good point to start your night, and we would love to have you with us! <br>
-    Enter is free, but it is always better to <a href="{{ route('guestlist') }}"><b>add your names to the guest list</b></a> because you know that full is full.
-</p>
 
 <p>- also this year CTS Records presents DJs Support Ukraine Project. <br>
     It will include series of short interviews of artists from the worldwide electronic music scene with words of support to their Ukrainian colleagues and all Ukrainian people. <br>
@@ -30,7 +20,7 @@
     <b>Why do you support Ukraine?</b> <br>
     and <br>
     <b>What you'd like to wish to Ukrainian DJs and electronic musicians in such a hard time of war in Ukraine?</b> <br>
-    And sure you can do it even if you are not a DJ. We will post the answers with your picture on our social media pages after ADE.
+    And sure you can do it even if you are not a DJ.
 </p>
 
 <p>Kind regards,</p>
